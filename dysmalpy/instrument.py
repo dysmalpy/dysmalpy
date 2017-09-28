ce0770da66c281c0301dae46f87fba2c755c5ba0
ttshimiz <tshimizu@astro.umd.edu>
2017-09-28 13:29:08 +0200

    create instrument.py

---
+++ dysmalpy/instrument.py
@@ -0,0 +1,9 @@
+# coding=utf8
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+#
+# File containing classes for defining different instruments
+# to "observe" a model galaxy.
+
+
+class Instrument:
+	pass
