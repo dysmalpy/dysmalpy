7b5f52813cb6b368a619b0231d91fbd5b900c4e4
ttshimiz <tshimizu@astro.umd.edu>
2017-10-06 12:29:11 +0200

    better handling of error arrays, add some attributes for 2D and 3D class

--- dysmalpy/data.py
+++ dysmalpy/data.py
@@ -12,12 +12,12 @@ import logging
 
 # Third party imports
 import numpy as np
-from spectral_cube import SpectralCube, BooleanArrayMask
 
 
 # LOGGER SETTINGS
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('DysmalPy')
+
 
 # Base Class for a data container
 class Data:
@@ -30,13 +30,11 @@ class Data:
         self.shape = shape
 
 
-
 class Data1D(Data):
 
     def __init__(self, r, velocity, vel_err=None, vel_disp=None,
                  vel_disp_err=None, mask=None, aper_size=None,
-                 aper_pa=None, error_frac=0.2):
-
+                 aper_pa=None, estimate_err=False, error_frac=0.2):
 
         if r.shape != velocity.shape:
             raise ValueError("r and velocity are not the same size.")
@@ -46,39 +44,56 @@ class Data1D(Data):
                 raise ValueError("mask and velocity are not the same size.")
 
         data = {'velocity': np.ma.masked_array(velocity, mask=mask)}
+
         if vel_disp is None:
+
             data['dispersion'] = None
+
         else:
+
             if vel_disp.shape != velocity.shape:
                 raise ValueError("vel_disp and velocity are not the same size.")
+
             data['dispersion'] = np.ma.masked_array(vel_disp, mask=mask)
 
-        # Default to error_frac of the data for errors if an
-        # error array isn't given.
-        if vel_err is None:
-            logger.info(r"No error array found for velocity, "
-                        r"using {:.0f}% of the data.".format(error_frac*100))
+        # Override any array given to vel_err if estimate_err is True
+        if estimate_err:
             vel_err = error_frac*velocity
+
+        if vel_err is None:
+
+            error = {'velocity': None}
+
         else:
+
             if vel_err.shape != velocity.shape:
-                raise ValueError("vel_err and velocity are not the same size.")
+                raise ValueError("vel_err and velocity are not the "
+                                 "same size.")
 
-
-        error = {'velocity': np.ma.masked_array(vel_err, mask=mask)}
+            error = {'velocity': np.ma.masked_array(vel_err, mask=mask)}
 
         if (vel_disp is not None) and (vel_disp_err is None):
-            logger.info(r"No error array found for dispersion, "
-                        r"using {:.0f}\% of the data.".format(error_frac * 100))
-            vel_disp_err = error_frac*vel_disp
-            error['dispersion'] = np.ma.masked_array(vel_disp_err, mask=mask)
-        elif (vel_disp is not None) and (vel_disp_err is not None):
-            if vel_disp_err.shape != velocity.shape:
-                raise ValueError("vel_disp_err and velocity are not the"
-                                 " same size.")
-            else:
+
+            if estimate_err:
+
+                vel_disp_err = error_frac * vel_disp
                 error['dispersion'] = np.ma.masked_array(vel_disp_err,
                                                          mask=mask)
+            else:
+
+                error['dispersion'] = None
+
+        elif (vel_disp is not None) and (vel_disp_err is not None):
+
+            if vel_disp_err.shape != velocity.shape:
+
+                raise ValueError("vel_disp_err and velocity are not the"
+                                 " same size.")
+
+            error['dispersion'] = np.ma.masked_array(vel_disp_err,
+                                                     mask=mask)
         else:
+
             error['dispersion'] = None
 
         shape = velocity.shape
@@ -88,53 +103,75 @@ class Data1D(Data):
         super(Data1D, self).__init__(data=data, error=error, ndim=1,
                                      shape=shape)
 
+
 class Data2D(Data):
 
     def __init__(self, pixscale, velocity, vel_err=None, vel_disp=None,
-                 vel_disp_err=None, mask=None, error_frac=0.2):
-
+                 vel_disp_err=None, mask=None, estimate_err=False,
+                 error_frac=0.2, ra=None, dec=None, ref_pixel=None):
 
         if mask is not None:
             if mask.shape != velocity.shape:
                 raise ValueError("mask and velocity are not the same size.")
 
         data = {'velocity': np.ma.masked_array(velocity, mask=mask)}
+
         if vel_disp is None:
+
             data['dispersion'] = None
+
         else:
+
             if vel_disp.shape != velocity.shape:
                 raise ValueError("vel_disp and velocity are not the same size.")
+
             data['dispersion'] = np.ma.masked_array(vel_disp, mask=mask)
 
-        # Default to error_frac of the data for errors if an
-        # error array isn't given.
-        if vel_err is None:
-            logger.info(r"No error array found for velocity, "
-                        r"using {:.0f}% of the data.".format(error_frac * 100))
-            vel_err = error_frac * velocity
-        else:
-            if vel_err.shape != velocity.shape:
-                raise ValueError("vel_err and velocity are not the same size.")
+        # Override any array given to vel_err if estimate_err is True
+        if estimate_err:
+            vel_err = error_frac*velocity
 
-        error = {'velocity': np.ma.masked_array(vel_err, mask=mask)}
+        if vel_err is None:
+
+            error = {'velocity': None}
+
+        else:
+
+            if vel_err.shape != velocity.shape:
+                raise ValueError("vel_err and velocity are not the "
+                                 "same size.")
+
+            error = {'velocity': np.ma.masked_array(vel_err, mask=mask)}
 
         if (vel_disp is not None) and (vel_disp_err is None):
-            logger.info(r"No error array found for dispersion, "
-                        r"using {:.0f}\% of the data.".format(error_frac * 100))
-            vel_disp_err = error_frac * vel_disp
-            error['dispersion'] = np.ma.masked_array(vel_disp_err, mask=mask)
-        elif (vel_disp is not None) and (vel_disp_err is not None):
-            if vel_disp_err.shape != velocity.shape:
-                raise ValueError("vel_disp_err and velocity are not the"
-                                 " same size.")
-            else:
+
+            if estimate_err:
+
+                vel_disp_err = error_frac * vel_disp
                 error['dispersion'] = np.ma.masked_array(vel_disp_err,
                                                          mask=mask)
+            else:
+
+                error['dispersion'] = None
+
+        elif (vel_disp is not None) and (vel_disp_err is not None):
+
+            if vel_disp_err.shape != velocity.shape:
+
+                raise ValueError("vel_disp_err and velocity are not the"
+                                 " same size.")
+
+            error['dispersion'] = np.ma.masked_array(vel_disp_err,
+                                                     mask=mask)
         else:
+
             error['dispersion'] = None
 
         shape = velocity.shape
         self.pixscale = pixscale
+        self.ra = ra
+        self.dec = dec
+        self.ref_pixel = ref_pixel
         super(Data2D, self).__init__(data=data, error=error, ndim=2,
                                      shape=shape)
 
@@ -142,7 +179,8 @@ class Data2D(Data):
 class Data3D(Data):
 
     def __init__(self, cube, pixscale, spec_type, spec_arr,
-                 err_cube=None, mask=None, estimate_err=False, error_frac=0.2):
+                 err_cube=None, mask=None, estimate_err=False, error_frac=0.2,
+                 ra=None, dec=None, ref_pixel=None):
 
         if mask is not None:
             if mask.shape != cube.shape:
@@ -179,6 +217,8 @@ class Data3D(Data):
         self.spec_type = spec_type
         self.spec_arr = spec_arr
         self.spec_step = spec_step
+        self.ra = ra
+        self.dec = dec
+        self.re_pixel = ref_pixel
         super(Data3D, self).__init__(data=data, error=error, ndim=3,
                                      shape=shape)
-
