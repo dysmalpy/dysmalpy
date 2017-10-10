1e5988e8e0b00dc8e1ac9c7901c47983bbc11009
ttshimiz <tshimizu@astro.umd.edu>
2017-10-10 13:34:53 +0200

    start writing create_model_data for Galaxy object

--- dysmalpy/galaxy.py
+++ dysmalpy/galaxy.py
@@ -18,7 +18,8 @@ import scipy.interpolate as scp_interp
 # Local imports
 # Package imports
 from .instrument import Instrument
-from .models import ModelSet
+from .models import ModelSet, calc_1dprofile
+from .data_classes import Data1D, Data2D, Data3D
 
 __all__ = ['Galaxy']
 
@@ -68,6 +69,115 @@ class Galaxy:
         if new_cosmo is None:
             self._cosmo = _default_cosmo
         self._cosmo = new_cosmo
+
+    def create_model_data(self, ndim_final=None, nx_sky=None, ny_sky=None,
+                          rstep=None, spec_type=None, spec_step=None,
+                          spec_start=None, nspec=None, line_center=None,
+                          spec_unit=None, aper_centers=None, slit_width=None,
+                          slit_pa=None, from_instrument=True, from_data=True):
+
+        """Simulate an IFU cube then optionally collapse it down to a 2D
+        velocity/dispersion field or 1D velocity/dispersion profile."""
+
+        if from_data:
+            ndim_final = self.data.ndim
+
+            if ndim_final == 3:
+
+                nx_sky = self.data.shape[2]
+                ny_sky = self.data.shape[1]
+                nspec = self.data.shape[0]
+                spec_ctype = self.data.data.wcs.wcs.ctype[-1]
+                if spec_ctype == 'WAVE':
+                    spec_type = 'wavelength'
+                elif spec_ctype == 'VOPT':
+                    spec_type = 'velocity'
+                spec_start = self.data.data.spectral_axis[0].value
+                spec_unit = self.data.data.spectral_axis.unit
+                spec_step = (self.data.data.spectral_axis[1].value -
+                             self.data.data.spectral_axis[0].value)
+                rstep = self.data.data.wcs.wcs.cdelt[0]*3600.
+
+
+
+
+
+        sim_cube, spec = self.model.simulate_cube(nx_sky=nx_sky, ny_sky=ny_sky,
+                                                  dscale=dscale, rstep=rstep,
+                                                  spec_type=spec_type,
+                                                  spec_step=spec_step,
+                                                  nspec=nspec,
+                                                  spec_start=spec_start,
+                                                  wave_center=line_center)
+
+        # Save the model cube whether or not it is the final form of the model
+        # data.
+        self.model_cube = Data3D(cube=sim_cube, pixscale=rstep,
+                                 spec_type=spec_type, spec_arr=spec,
+                                 spec_unit=spec_unit)
+
+        if ndim_final == 3:
+
+            self.model_data = Data3D(cube=sim_cube, pixscale=rstep,
+                                     spec_type=spec_type, spec_arr=spec,
+                                     spec_unit=spec_unit)
+
+        elif ndim_final == 2:
+
+            if spec_type == "velocity":
+
+                vel = self.model_cube.data.moment1().value
+                disp = self.model_cube.data.linewidth_sigma().value
+
+            elif spec_type == "wavelength":
+
+                cube_with_vel = self.model_cube.data.with_spectral_unit(
+                    u.km/u.s, velocity_convention='optical',
+                    rest_value=wave_center*spec_unit)
+
+                vel = cube_with_vel.moment1().value
+                disp = cube_with_vel.linewidth_sigma().value
+
+            else:
+                raise ValueError("spec_type can only be 'velocity' or "
+                                 "'wavelength.'")
+
+            self.model_data = Data2D(pixscale=rstep, velocity=vel,
+                                     vel_disp=disp)
+
+        elif ndim_final == 1:
+
+            if spec_type == 'wavelength':
+
+                cube_with_vel = self.model_cube.data.with_spectral_unit(
+                    u.km / u.s, velocity_convention='optical',
+                    rest_value=wave_center * spec_unit)
+
+                cube_data = cube_with_vel.unmasked_data[:]
+                vel_arr = cube_with_vel.spectral_axis.to(u.km/u.s).value
+
+            elif spec_type == 'velocity':
+
+                cube_data = sim_cube
+                vel_arr = spec
+
+            else:
+                raise ValueError("spec_type can only be 'velocity' or "
+                                 "'wavelength.'")
+
+            r1d, vel1d, disp1d = calc_1dprofile(cube_data, slit_width,
+                                                slit_pa, rstep, vel_arr)
+
+            vinterp = scp_interp.interp1d(r1d, vel1d,
+                                          fill_value='extrapolate')
+            disp_interp = scp_interp.interp1d(r1d, disp1d,
+                                              fill_value='extrapolate')
+            vel1d = vinterp(aper_centers)
+            disp1d = disp_interp(aper_centers)
+
+            self.model_data = Data1D(r=aper_centers, velocity=vel1d,
+                                     vel_disp=disp1d, slit_width=slit_width,
+                                     slit_pa=slit_pa)
 
 
 
