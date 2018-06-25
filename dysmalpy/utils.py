e2c474ddcc1be95f91ad03290e60bbcf399b92fb
ttshimiz <tshimizu@astro.umd.edu>
2018-06-25 11:12:56 +0200

    add "debug" option
    Right now only works for 1D profile measurement and all it does is plot the line fit for every aperture

--- dysmalpy/utils.py
+++ dysmalpy/utils.py
@@ -153,7 +153,7 @@ def calc_pix_position(r, pa, xcenter, ycenter):
 
 def measure_1d_profile_apertures(cube, rap, pa, spec_arr, dr=None, center_pixel=None,
                                  ap_centers=None, spec_mask=None, estimate_err=False, nmc=100,
-                                 profile_direction='positive'):
+                                 profile_direction='positive', debug=False):
     """
     Measure the 1D rotation curve using equally spaced apertures along a defined axis
     :param cube: Cube to measure the 1D profile on
@@ -238,6 +238,12 @@ def measure_1d_profile_apertures(cube, rap, pa, spec_arr, dr=None, center_pixel=
         mean[i] = best_fit.mean.value
         disp[i] = best_fit.stddev.value
         flux[i] = best_fit.amplitude.value * np.sqrt(2 * np.pi) * disp[i]
+
+        if debug:
+            plt.figure()
+            plt.plot(spec_arr_fit, spec_fit)
+            plt.plot(spec_arr_fit, best_fit(spec_arr_fit))
+            plt.title('r = {0}'.format(ap_centers[i]))
 
         if estimate_err:
             residual = spec - best_fit(spec)
