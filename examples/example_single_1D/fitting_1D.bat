28b8d0eca1900f3d1530b9b70701fea9809e8e8e
ttshimiz <tshimizu@astro.umd.edu>
2019-01-09 09:56:50 +0000

    added example batch files for running on Windows

---
+++ examples/example_single_1D/fitting_1D.bat
@@ -0,0 +1,4 @@
+@CALL "C:\Programme\Anaconda3\Scripts\activate.bat"
+set PYTHONPATH=C:\Programme\dysmalpy
+
+python C:\Programme\dysmalpy\fitting_wrappers\dysmalpy_fit_single_1D.py fitting_1D.params
---
+++ examples/example_single_2D/fitting_2D.bat
@@ -0,0 +1,4 @@
+@CALL "C:\Programme\Anaconda3\Scripts\activate.bat"
+set PYTHONPATH=C:\Programme\dysmalpy
+
+python C:\Programme\dysmalpy\fitting_wrappers\dysmalpy_fit_single_2D.py fitting_2D.params
