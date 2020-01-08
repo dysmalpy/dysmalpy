ce01b56e06fde68360a1cf238f7a3e21ddb550d9
Sedona Price <sedona.price@gmail.com>
2020-01-08 14:55:07 +0100

    Update plotting.py

--- dysmalpy/plotting.py
+++ dysmalpy/plotting.py
@@ -1618,9 +1618,13 @@ def show_1d_apers_plot(ax, gal, data1d, data2d, galorig=None, alpha_aper=0.8, re
     slit_width = data1d.slit_width
     slit_pa = data1d.slit_pa
     rstep = gal.instrument.pixscale.value
+    try:
+        rstep1d = gal.instrument1d.pixscale.value
+    except:
+        rstep1d = rstep
     rpix = slit_width/rstep/2.
     aper_dist_pix = 2*rpix
-    aper_centers_pix = aper_centers/rstep
+    aper_centers_pix = aper_centers/rstep1d
 
     pa = slit_pa
 
@@ -1672,7 +1676,11 @@ def show_1d_apers_plot(ax, gal, data1d, data2d, galorig=None, alpha_aper=0.8, re
 
     # First determine the centers of all the apertures that fit within the cube
     xaps, yaps = calc_pix_position(aper_centers_pix, pa, center_pixel[0], center_pixel[1])
-
+    
+    print("aper_centers_pix={}".format(aper_centers_pix))
+    print("xaps={}".format(xaps))
+    print("yaps={}".format(yaps))
+    
     pyoff = 0.
 
     cmstar = cm.plasma
