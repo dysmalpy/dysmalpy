948be24107d318ca91695b56051db23e8b1efa81
Sedona Price <sedona.price@gmail.com>
2020-10-14 18:58:33 +0200

    Adding some wrapper options to help with tutorial Jupyter notebooks -- top-level specification of datadir/outdir without overwriting / copying param file

--- fitting_wrappers/dysmalpy_fit_single_3D.py
+++ fitting_wrappers/dysmalpy_fit_single_3D.py
@@ -93,10 +93,16 @@ def dysmalpy_fit_single_3D_wrapper(param_filename=None, default_load_data=True):
     
     return None
 
-def dysmalpy_fit_single_3D(param_filename=None, data=None):
+def dysmalpy_fit_single_3D(param_filename=None, data=None, datadir=None, outdir=None):
     
     # Read in the parameters from param_filename:
     params = utils_io.read_fitting_params(fname=param_filename)
+    
+    # OVERRIDE SETTINGS FROM PARAMS FILE if passed directly -- eg from an example Jupyter NB:
+    if datadir is not None:
+        params['datadir'] = datadir
+    if outdir is not None:
+        params['outdir'] = outdir
     
     # Setup some paths:
     outdir = utils_io.ensure_path_trailing_slash(params['outdir'])
