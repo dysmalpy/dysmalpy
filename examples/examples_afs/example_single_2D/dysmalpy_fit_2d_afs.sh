a1352e97fddd2477718358d467e7948bb7982fae
Sedona Price <sedona.price@gmail.com>
2020-10-26 23:02:35 +0100

    Moving submodules; Version 1.0.0 release

--- examples/examples_afs/example_single_2D/dysmalpy_fit_2d_afs.sh
+++ examples/examples_afs/example_single_2D/dysmalpy_fit_2d_afs.sh
@@ -8,7 +8,4 @@ source /afs/mpe.mpg.de/astrosoft/dysmalpy/dysmalpy_setup.sh
 # Run fitting
 export DPY_PATH='/afs/mpe.mpg.de/astrosoft/dysmalpy/dysmalpy'
 
-python $DPY_PATH/fitting_wrappers/dysmalpy_fit_single_2D.py $1
-
-
-
+python $DPY_PATH/dysmalpy/fitting_wrappers/dysmalpy_fit_single_2D.py $1
