81fccca2db75ff2a26cf462c0b438bfd64bfcd1f
Sedona Price <sedona.price@gmail.com>
2021-02-12 21:11:10 +0100

    Update fitting.py

--- dysmalpy/fitting.py
+++ dysmalpy/fitting.py
@@ -693,11 +693,12 @@ def _fit_emcee_3(gal, **kwargs ):
                 logger.warning("overwrite={} & File already exists! Will not save file. \n {}".format(kwargs_fit['overwrite'], fname))
 
         # Return early if it won't save the results, sampler:
-        if os.path.isfile(kwargs_fit['f_results']):
-            msg = "overwrite={}, and 'f_results' won't be saved,".format(kwargs_fit['overwrite'])
-            msg += " so the fit will not be saved.\n Specify new outfile or delete old files."
-            logger.warning(msg)
-            return None
+        if kwargs_fit['f_results'] is not None:
+            if os.path.isfile(kwargs_fit['f_results']):
+                msg = "overwrite={}, and 'f_results' won't be saved,".format(kwargs_fit['overwrite'])
+                msg += " so the fit will not be saved.\n Specify new outfile or delete old files."
+                logger.warning(msg)
+                return None
 
         # Check length of sampler:
         if os.path.isfile(kwargs_fit['f_sampler']):
@@ -705,11 +706,12 @@ def _fit_emcee_3(gal, **kwargs ):
 
             try:
                 if backend.get_chain().shape[0] >= kwargs_fit['nSteps']:
-                    if os.path.isfile(kwargs_fit['f_results']):
-                        msg = "overwrite={}, and 'f_sampler' already contains {} steps,".format(kwargs_fit['overwrite'], backend.get_chain().shape[0])
-                        msg += " so the fit will not be saved.\n Specify new outfile or delete old files."
-                        logger.warning(msg)
-                        return None
+                    if kwargs_fit['f_results'] is not None:
+                        if os.path.isfile(kwargs_fit['f_results']):
+                            msg = "overwrite={}, and 'f_sampler' already contains {} steps,".format(kwargs_fit['overwrite'], backend.get_chain().shape[0])
+                            msg += " so the fit will not be saved.\n Specify new outfile or delete old files."
+                            logger.warning(msg)
+                            return None
                 else:
                     pass
             except:
@@ -1080,11 +1082,12 @@ def fit_mpfit(gal, **kwargs):
                     logger.warning("overwrite={} & File already exists! Will not save file. \n {}".format(kwargs_fit['overwrite'], fname))
 
         # Return early if it won't save the results, sampler:
-        if os.path.isfile(kwargs_fit['f_results']):
-            msg = "overwrite={}, and 'f_results' won't be saved,".format(kwargs_fit['overwrite'])
-            msg += " so the fit will not be saved.\n Specify new outfile or delete old files."
-            logger.warning(msg)
-            return None
+        if kwargs_fit['f_results'] is not None:
+            if os.path.isfile(kwargs_fit['f_results']):
+                msg = "overwrite={}, and 'f_results' won't be saved,".format(kwargs_fit['overwrite'])
+                msg += " so the fit will not be saved.\n Specify new outfile or delete old files."
+                logger.warning(msg)
+                return None
     # ---------------------------------------------------
 
     # Setup file redirect logging:
