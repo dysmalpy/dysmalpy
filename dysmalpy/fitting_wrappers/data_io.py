3D masking / plotting upgrades / fixes
- Adding some loading routines, masking wrappers
- Added some docstrings

dysmalpy/fitting_wrappers/data_io.py
@@ -28,7 +28,7 @@ from dysmalpy import config
 
 import astropy.io.fits as fits
 
-from dysmalpy.fitting_wrappers.utils_calcs import auto_gen_3D_mask, _auto_truncate_crop_cube
+from dysmalpy.fitting_wrappers.utils_calcs import auto_gen_3D_mask, _auto_truncate_crop_cube, pad_3D_mask_to_uncropped_size
 from dysmalpy.fitting_wrappers.setup_gal_models import setup_data_weighting_method
 
 def read_fitting_params_input(fname=None):
@@ -195,6 +195,8 @@ def read_fitting_params(fname=None):
     return params
 
 
+
+
 def save_results_ascii_files(fit_results=None, gal=None, params=None, overwrite=False):
     filename_extra = ''
     if 'filename_extra' in params.keys():
@@ -353,7 +355,7 @@ def load_single_object_2D_data(params=None, adjust_error=False,
             skip_crop=False, datadir=None):
 
     # +++++++++++++++++++++++++++++++++++++++++++
-    # Upload the data set to be fit
+    # Load the data set to be fit
 
 
     # Setup datadir, if set. If not set (so datadir=None), fdata must be the full path.
@@ -572,12 +574,12 @@ def load_single_object_2D_data(params=None, adjust_error=False,
 
     return data2d
 
-#
-def load_single_object_3D_data(params=None, datadir=None):
 
-
+def load_single_object_3D_data(params=None, datadir=None,
+            skip_automask=False, skip_auto_truncate_crop=False,
+            return_crop_info=False):
     # +++++++++++++++++++++++++++++++++++++++++++
-    # Upload the data set to be fit
+    # Load the data set to be fit
 
     # Setup datadir, if set. If not set (so datadir=None), fdata must be the full path.
     if datadir is None:
@@ -588,7 +590,6 @@ def load_single_object_3D_data(params=None, datadir=None):
         if datadir is None:
             datadir = ''
 
-
     cube = fits.getdata(datadir+params['fdata_cube'])
     err_cube = fits.getdata(datadir+params['fdata_err'])
     header = fits.getheader(datadir+params['fdata_cube'])
@@ -596,18 +597,36 @@ def load_single_object_3D_data(params=None, datadir=None):
     mask = None
     if 'fdata_mask' in params.keys():
         if params['fdata_mask'] is not None:
-            mask = fits.getdata(datadir+params['fdata_mask'])
+            # Check if it's full path:
+            fdata_mask = params['fdata_mask']
+            if not os.path.isfile(fdata_mask):
+                # Otherwise try datadir:
+                fdata_mask = datadir+params['fdata_mask']
+            if os.path.isfile(fdata_mask):
+                mask = fits.getdata(fdata_mask)
+                # Crop cube: first check if masks match the cubes already or not -- otherwise load later
+                if mask.shape != cube.shape:
+                    #mask = None
+                    raise ValueError
 
-    #
+
     mask_sky=None
     if 'fdata_mask_sky' in params.keys():
         if params['fdata_mask_sky'] is not None:
             mask_sky = fits.getdata(datadir+params['fdata_mask_sky'])
-    #
+            # Crop cube: first check if masks match the cubes already or not -- otherwise load later
+            if mask_sky.shape != cube.shape[1:3]:
+                #mask_sky = None
+                raise ValueError
+
     mask_spec=None
     if 'fdata_mask_spec' in params.keys():
         if params['fdata_mask_spec'] is not None:
             mask_spec = fits.getdata(datadir+params['fdata_mask_spec'])
+            # Crop cube: first check if masks match the cubes already or not -- otherwise load later
+            if mask_spec.shape[0] != cube.shape[0]:
+                #mask_spec = None
+                raise ValueError
 
 
     if 'weighting_method' in params.keys():
@@ -624,7 +643,6 @@ def load_single_object_3D_data(params=None, datadir=None):
         ycenter = params['ycenter']
     else:
         ycenter = None
-
 
 
     ####################################
@@ -674,42 +692,63 @@ def load_single_object_3D_data(params=None, datadir=None):
         elif header['CUNIT3'].strip().upper() == 'MICRON':
             raise ValueError('Assumed unit was km/s -- but does not match the cube header! CUNIT3={}'.format(header['CUNIT3']))
 
-
-
-
-    pscale = np.abs(header['CDELT1']) * 3600.    # convert from deg CDELT1 to arcsec
-
-    ####################################
-
-
-    cube, err_cube, mask, mask_sky, mask_spec, gal_weight, spec_arr, xcenter, ycenter = _auto_truncate_crop_cube(cube,
-                                            params=params,
-                                            pixscale=pscale,
-                                            spec_type='velocity', spec_arr=spec_arr,
-                                            err_cube=err_cube, mask_cube=mask,
-                                            mask_sky=mask_sky, mask_spec=mask_spec,
-                                            spec_unit=spec_unit,weight=gal_weight,
-                                            xcenter=xcenter, ycenter=ycenter)
-
+    if header['CUNIT1'].strip().upper() in ['DEGREE', 'DEG']:
+        pscale = np.abs(header['CDELT1']) * 3600.    # convert from deg CDELT1 to arcsec
+    elif header['CUNIT1'].strip().upper() in ['ARCSEC']:
+        pscale = np.abs(header['CDELT1'])
 
     ####################################
-    if (mask is None) & ('auto_gen_3D_mask' in params.keys()):
+
+    cube_precrop_sh = cube.shape
+    wh_spec_keep = wh_ends_trim = sp_trm = None
+    if (not skip_auto_truncate_crop):
+        cube, err_cube, mask, mask_sky, mask_spec, \
+            gal_weight, spec_arr, xcenter, ycenter, \
+            wh_spec_keep, wh_ends_trim, sp_trm = _auto_truncate_crop_cube(cube,
+                                                params=params,
+                                                spec_type='velocity', spec_arr=spec_arr,
+                                                err_cube=err_cube, mask_cube=mask,
+                                                mask_sky=mask_sky, mask_spec=mask_spec,
+                                                spec_unit=spec_unit,weight=gal_weight,
+                                                xcenter=xcenter, ycenter=ycenter)
+
+
+    # # Try loading masks that weren't set earlier bc of size differences:
+    # if mask is None:
+    #     if 'fdata_mask' in params.keys():
+    #         if params['fdata_mask'] is not None:
+    #             # Check if it's full path:
+    #             fdata_mask = params['fdata_mask']
+    #             if not os.path.isfile(fdata_mask):
+    #                 # Otherwise try datadir:
+    #                 fdata_mask = datadir+params['fdata_mask']
+    #             if os.path.isfile(fdata_mask):
+    #                 mask = fits.getdata(fdata_mask)
+    #
+    # if mask is None:
+    #     if 'fdata_mask_sky' in params.keys():
+    #         if params['fdata_mask_sky'] is not None:
+    #             mask_sky = fits.getdata(datadir+params['fdata_mask_sky'])
+    # if mask_spec is None:
+    #     if 'fdata_mask_spec' in params.keys():
+    #         if params['fdata_mask_spec'] is not None:
+    #             mask_spec = fits.getdata(datadir+params['fdata_mask_spec'])
+
+    ####################################
+    if (mask is None) & ('auto_gen_3D_mask' in params.keys()) & (not skip_automask):
         if params['auto_gen_3D_mask']:
-            if 'auto_gen_mask_snr_thresh_1' not in params.keys():
-                params['auto_gen_mask_snr_thresh_1'] = params['auto_gen_mask_snr_thresh']
-            if 'auto_gen_mask_sky_var_thresh' not in params.keys():
-                params['auto_gen_mask_sky_var_thresh'] = 3.
-            #mask = _auto_gen_3D_mask_simple(cube=cube, err=err_cube, snr_thresh=params['auto_gen_mask_snr_thresh'],
-            #        npix_min=params['auto_gen_mask_npix_min'])
-            mask = auto_gen_3D_mask(cube=cube, err=err_cube,
-                    sig_thresh=params['auto_gen_mask_sig_thresh'],
-                    #snr_thresh=params['auto_gen_mask_snr_thresh'],
-                    #snr_thresh_1 = params['auto_gen_mask_snr_thresh_1'],
-                    npix_min=params['auto_gen_mask_npix_min'],
-                    sky_var_thresh=params['auto_gen_mask_sky_var_thresh'])
+            if 'fdata_mask' in params.keys():
+                if not os.path.isfile(fdata_mask):
+                    print("Can't load mask from 'fdata_mask'={}, ".format(fdata_mask))
+                    print("  but 'auto_gen_3D_mask'={}, ".format(auto_gen_3D_mask))
+                    print("  so automatically generating mask")
 
-        else:
-            mask = np.ones(cube.shape)
+            mask = generate_3D_mask(cube=cube, err=err_cube, params=params)
+
+
+    # Catch final cases: skip_automask=True or 'auto_gen_3D_mask' not in params.keys():
+    if (mask is None):
+        mask = np.ones(cube.shape)
 
     ####################################
     # Mask NaNs:
@@ -741,10 +780,145 @@ def load_single_object_3D_data(params=None, datadir=None):
                                       smoothing_npix=smoothing_npix,
                                       xcenter=xcenter, ycenter=ycenter)
 
+
+    if return_crop_info:
+        data3d.cube_precrop_sh = cube_precrop_sh
+        data3d.wh_spec_keep = wh_spec_keep
+        data3d.wh_ends_trim = wh_ends_trim
+        data3d.sp_trm = sp_trm
+
     return data3d
 
 
+def generate_3D_mask(gal=None, cube=None, err=None, params=None,
+            sig_segmap_thresh=None, npix_segmap_min=None,
+            snr_int_flux_thresh=None, snr_thresh_pixel=None,
+            sky_var_thresh=None,
+            apply_skymask_first=None):
 
+    """
+    Generate a mask for a 3D cube, based on thresholds / other automated detections.
+
+    If values are not set, they are taken from the params settings.
+
+    See `~dysmalpy.fitting_wrappers.utils_calcs.auto_gen_3D_mask` for parameter explanations.
+
+    Input:
+        cube:               3D cube (numpy ndarray)
+        err:                3D error cube (numpy ndarray)
+
+        params:             Parameters setting dictionary
+
+    Optional input:
+        gal:                Galaxy instance. If cube or err is not set, will use
+                            the gal.data.data, gal.data.error cubes
+
+    Output:
+            mask (3D cube)
+    """
+    if cube is None:
+        cube = gal.data.data.unmasked_data[:].value * gal.data.mask
+    if err is None:
+        err = gal.data.error.unmasked_data[:].value
+
+    if snr_thresh_pixel is None:
+        if 'auto_gen_mask_snr_thresh_pixel' not in params.keys():
+            params['auto_gen_mask_snr_thresh_pixel'] = None
+        snr_thresh_pixel = params['auto_gen_mask_snr_thresh_pixel']
+
+    if sky_var_thresh is None:
+        if 'auto_gen_mask_sky_var_thresh' not in params.keys():
+            params['auto_gen_mask_sky_var_thresh'] = 3.
+        sky_var_thresh = params['auto_gen_mask_sky_var_thresh']
+
+    if snr_int_flux_thresh is None:
+        if 'auto_gen_mask_snr_int_flux_thresh' not in params.keys():
+            params['auto_gen_mask_snr_int_flux_thresh'] = 3.
+        snr_int_flux_thresh = params['auto_gen_mask_snr_int_flux_thresh']
+
+    if sig_segmap_thresh is None:
+        if 'auto_gen_mask_sig_segmap_thresh' not in params.keys():
+            params['auto_gen_mask_sig_segmap_thresh'] = 1.5
+        sig_segmap_thresh = params['auto_gen_mask_sig_segmap_thresh']
+
+    if npix_segmap_min is None:
+        if 'auto_gen_mask_npix_segmap_min' not in params.keys():
+            params['auto_gen_mask_npix_segmap_min'] = 5
+        npix_segmap_min = params['auto_gen_mask_npix_segmap_min']
+
+    if apply_skymask_first is None:
+        if 'auto_gen_mask_apply_skymask_first' not in params.keys():
+            params['auto_gen_mask_apply_skymask_first'] = True
+        apply_skymask_first = params['auto_gen_mask_apply_skymask_first']
+
+
+    msg =  "++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+    msg += "  Creating 3D auto mask with the following settings: \n"
+    msg += "        sig_segmap_thresh   = {} \n".format(sig_segmap_thresh)
+    msg += "        npix_segmap_min     = {} \n".format(npix_segmap_min)
+    msg += "        snr_int_flux_thresh = {} \n".format(snr_int_flux_thresh)
+    msg += "        snr_thresh_pixel    = {} \n".format(snr_thresh_pixel)
+    msg += "        sky_var_thresh      = {} \n".format(sky_var_thresh)
+    msg += "        apply_skymask_first = {} \n".format(apply_skymask_first)
+    msg += "++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+    print(msg)
+
+    return auto_gen_3D_mask(cube=cube, err=err,
+                    sig_segmap_thresh=sig_segmap_thresh, npix_segmap_min=npix_segmap_min,
+                    snr_int_flux_thresh=snr_int_flux_thresh, snr_thresh_pixel = snr_thresh_pixel,
+                    sky_var_thresh=sky_var_thresh, apply_skymask_first=apply_skymask_first)
+
+
+
+
+def save_3D_mask(gal=None, mask=None, filename=None, overwrite=False, save_uncropped_size=True):
+
+    """
+    Generate a mask for a 3D cube, based on thresholds / other automated detections.
+
+    If values are not set, they are taken from the params settings.
+
+    See `~dysmalpy.fitting_wrappers.utils_calcs.auto_gen_3D_mask` for parameter explanations.
+
+    Input:
+        cube:               3D cube (numpy ndarray)
+        err:                3D error cube (numpy ndarray)
+
+        params:             Parameters setting dictionary
+
+    Optional input:
+        gal:                Galaxy instance. If cube or err is not set, will use
+                            the gal.data.data, gal.data.error cubes
+
+        save_uncropped_size:  If gal.data has info about pre-cropped size, will save the
+                                mask w/ zero padding to match the original data cube size (eg, for saving)
+
+    Output:
+            mask (3D cube)
+    """
+    mask_cube = mask.copy()
+    if gal is not None:
+        if gal.data.data.wcs.wcs.cunit[1].to_string().upper().strip() in ['DEGREE', 'DEG']:
+            pscale = np.abs(gal.data.data.wcs.wcs.cdelt[0]) * 3600.    # convert from deg CDELT1 to arcsec
+        elif gal.data.data.wcs.wcs.cunit[1].to_string().upper().strip() in ['ARCSEC']:
+            pscale = np.abs(gal.data.data.wcs.wcs.cdelt[0])
+        pixscale=pscale
+        if gal.data.data.wcs.wcs.ctype[2] == 'VOPT':
+            spec_type = 'velocity'
+        elif gal.data.data.wcs.wcs.ctype[2] == 'WAVE':
+            spec_type = 'wavelength'
+
+        spec_unit=u.Unit(gal.data.data.wcs.wcs.cunit[2].to_string())
+        spec_arr = gal.data.data.spectral_axis.to(spec_unit).value
+
+        if save_uncropped_size:
+            mask_cube, spec_arr  = pad_3D_mask_to_uncropped_size(gal=gal, mask=mask)
+
+    mask_cube = data_classes.Data3D(cube=mask_cube, pixscale=pixscale,
+                        spec_type=spec_type, spec_arr=spec_arr, spec_unit=spec_unit)
+    mask_cube.data.write(filename, overwrite=overwrite)
+
+    return None
 
 def ensure_path_trailing_slash(path):
     if (path[-1] != '/'):
@@ -752,8 +926,9 @@ def ensure_path_trailing_slash(path):
     return path
 
 ####
-def get_ndim_fit_from_paramfile(param_filename=None):
-    params = read_fitting_params(fname=param_filename)
+def get_ndim_fit_from_paramfile(params=None, param_filename=None):
+    if params is None:
+        params = read_fitting_params(fname=param_filename)
 
     ndim_fit = None
 

dysmalpy/fitting_wrappers/utils_calcs.py
@@ -20,7 +20,154 @@ except:
     loaded_photutils = False
 
 
-def _auto_gen_3D_mask_simple(cube=None, err=None, snr_thresh=3.):
+
+def auto_gen_3D_mask(cube=None, err=None,
+            sig_segmap_thresh=1.5, npix_segmap_min=5,
+            snr_int_flux_thresh=3.,
+            snr_thresh_pixel=None,
+            sky_var_thresh=3.,
+            apply_skymask_first=True):
+    """
+    Generate a mask for a 3D cube, based on thresholds / other automated detections.
+
+    Input:
+        cube:               3D cube (numpy ndarray)
+        err:                3D error cube (numpy ndarray)
+
+    Optional input:
+        sig_segmap_thresh:  If photutils is available, use segmentation map on the
+                            integrated 2D spaxel flux map to determine the spatial masking region.
+                            Default: sig_segmap_thresh = 1.5
+        npix_segmap_min:    Minimum number of pixels in each segmap grouping.
+
+        snr_int_flux_thresh:If photutils is unavailable, do a SNR threshold cut on the
+                            integrated 2D spaxel flux map to determine the spatial masking region.
+                            Default: snr_int_flux_thresh = 3.
+
+        snr_thresh_pixel:   Flat SNR threshold to apply to all pixels (eg, each point in x,y,spectral space).
+                            USE CAUTION IF SETTING, as can bias fits.
+                            Default: snr_thresh_pixel = None (no per-pixel SNR masking).
+
+        sky_var_thresh:     Threshold for determining skylines from variance spectrum.
+                            NaNs are masked in error cube, then variance spectrum is
+                            determined by spatial sum in quadrature of variance (error^2).
+                            Spectral regions where variance > sky_var_thresh * median(variance)
+                            are masked as skyline regions.
+                            Typical values: 3.0 for K/Y, 2.0 for H/J. (Tune as necessary.)
+
+        apply_skymask_first:    Apply skyline mask before doing segmentation/integrated flux masking? Default: True
+
+    Output:
+            mask (3D cube)
+    """
+
+    # snr_int_flux_thresh=3., snr_thresh_pixel=3.,
+
+    ## Crude first-pass masking by pixel S/N:
+    if snr_thresh_pixel is not None:
+        mask_sn_pix = _auto_gen_3D_mask_pixel_SNR(cube=cube, err=err, snr_thresh=snr_thresh_pixel)
+    else:
+        mask_sn_pix = None
+
+
+    mask = np.ones(cube.shape)
+
+    cube_m = cube.copy()
+    ecube_m = err.copy()
+
+    ####################################
+    # Mask NaNs:
+    mask[~np.isfinite(cube_m)] = 0.
+    cube_m[~np.isfinite(cube_m)] = -99.
+
+    mask[~np.isfinite(ecube_m)] = 0.
+    ecube_m[~np.isfinite(ecube_m)] = -99.
+
+    # Clean up 0s in error, if it's masked
+    ecube_m[mask == 0] = 99.
+
+
+
+    if sky_var_thresh is not None:
+        # Mask skylines from variance:
+        mask_sky = _auto_gen_3D_mask_sky(ecube_m, mask=mask, sky_var_thresh=sky_var_thresh)
+    else:
+        mask_sky = None
+
+
+    ####################################
+    # Integrated 2D flux masking: either segmentation map, or SNR cut
+
+    # Premask sky:
+    if (apply_skymask_first & (mask_sky is not None)):
+        mask *= mask_sky
+
+    do_int_flux_SNR_mask = False
+    mask2D = None
+    fmap_cube_sn = np.sum(cube_m*mask, axis=0)
+    emap_cube_sn = np.sqrt(np.sum(ecube_m**2*mask, axis=0))
+    if sig_segmap_thresh is not None:
+        # Do segmap on mask2D?????
+        if loaded_photutils:
+            bkg = None
+            exclude_percentile=0.
+            while ((bkg is None) & (exclude_percentile < 100.)):
+                exclude_percentile += 5.
+                try:
+                    bkg = photutils.Background2D(fmap_cube_sn, fmap_cube_sn.shape, filter_size=(3,3),
+                                    exclude_percentile=exclude_percentile)
+                except:
+                    pass
+            if bkg is None:
+                raise ValueError
+            else:
+                if exclude_percentile > 10.:
+                    print(" Masking segmap: used exclude_percentile={}".format(exclude_percentile))
+
+            thresh = sig_segmap_thresh * bkg.background_rms
+
+            kernel = Gaussian2DKernel(3. /(2. *np.sqrt(2.*np.log(2.))), x_size=5, y_size=5)   # Gaussian of FWHM 3 pix
+            segm = photutils.detect_sources(fmap_cube_sn, thresh, npixels=npix_segmap_min, filter_kernel=kernel)
+
+
+            mask2D = segm._data.copy()
+            mask2D[mask2D>0] = 1
+        else:
+            do_int_flux_SNR_mask = True
+
+
+    elif snr_int_flux_thresh is not None:
+        do_int_flux_SNR_mask = True
+
+    if do_int_flux_SNR_mask:
+        # TRY JUST S/N cut on 2D integrated flux?
+        sn_map_cube_sn = fmap_cube_sn / emap_cube_sn
+        # Handle nans / pre-masked
+        sn_map_cube_sn[~np.isfinite(sn_map_cube_sn)] = 0
+
+        mask2D = np.ones(sn_map_cube_sn.shape)
+        mask2D[sn_map_cube_sn < snr_int_flux_thresh] = 0
+
+    if mask2D is not None:
+        # Apply mask2D to mask:
+        mask_cube = np.tile(mask2D, (cube_m.shape[0], 1, 1))
+    else:
+        mask_cube = None
+
+
+
+    if mask_sn_pix is not None:
+        mask *= mask_sn_pix
+
+    if mask_cube is not None:
+        mask *= mask_cube
+
+    if mask_sky is not None:
+        mask *= mask_sky
+
+    return mask
+
+def _auto_gen_3D_mask_pixel_SNR(cube=None, err=None, snr_thresh=3.):
     # Crude first-pass on auto-generated 3D cube mask, based on S/N:
 
     snr_cube = np.abs(cube)/np.abs(err)
@@ -44,117 +191,33 @@ def _auto_gen_3D_mask_sky(ecube, mask=None, sky_var_thresh=None):
 
     return mask_sky
 
-def auto_gen_3D_mask(cube=None, err=None,
-            sig_thresh=1.5, npix_min=5,
-            snr_thresh=3., 
-            snr_thresh_1=None,
-            sky_var_thresh=3.):
-
-    # snr_thresh=3., snr_thresh_1=3.,
-
-    ## Crude first-pass masking by pixel S/N:
-    if snr_thresh_1 is not None:
-        mask_sn_pix = _auto_gen_3D_mask_simple(cube=cube, err=err, snr_thresh=snr_thresh_1)
-    else:
-        mask_sn_pix = None
-
-
-    mask = np.ones(cube.shape)
-
-    cube_m = cube.copy()
-    ecube_m = err.copy()
-
-    ####################################
-    # Mask NaNs:
-    mask[~np.isfinite(cube_m)] = 0
-    cube_m[~np.isfinite(cube_m)] = -99.
-
-    mask[~np.isfinite(ecube_m)] = 0
-    ecube_m[~np.isfinite(ecube_m)] = -99.
-
-    # Clean up 0s in error, if it's masked
-    ecube_m[mask == 0] = 99.
-
-
-
-    if sky_var_thresh is not None:
-        # Mask skylines from variance:
-        mask_sky = _auto_gen_3D_mask_sky(ecube_m, mask=mask, sky_var_thresh=sky_var_thresh)
-        #mask *= mask_sky
-    else:
-        mask_sky = None
-
-
-    ####################################
-    if sig_thresh is not None:
-        fmap_cube_sn = np.sum(cube_m*mask, axis=0)
-        emap_cube_sn = np.sqrt(np.sum(ecube_m**2*mask, axis=0))
-
-
-        # Do segmap on mask2D?????
-        if loaded_photutils:
-
-            bkg = photutils.Background2D(fmap_cube_sn, fmap_cube_sn.shape, filter_size=(3,3))
-
-            thresh = sig_thresh * bkg.background_rms
-
-            #kernel = Gaussian2DKernel(2. /(2. *np.sqrt(2.*np.log(2.))), x_size=3, y_size=3)   # Gaussian of FWHM 2 pix
-            kernel = Gaussian2DKernel(3. /(2. *np.sqrt(2.*np.log(2.))), x_size=5, y_size=5)   # Gaussian of FWHM 3 pix
-            segm = photutils.detect_sources(fmap_cube_sn, thresh, npixels=npix_min, filter_kernel=kernel)
-
-
-            mask2D = segm._data.copy()
-            mask2D[mask2D>0] = 1
-        else:
-            # TRY JUST S/N cut on 2D?
-            sn_map_cube_sn = fmap_cube_sn / emap_cube_sn
-
-            mask2D = np.ones(sn_map_cube_sn.shape)
-            mask2D[sn_map_cube_sn < snr_thresh] = 0
-
-
-
-        # Apply mask2D to mask:
-        mask_cube = np.tile(mask2D, (cube_m.shape[0], 1, 1))
-
-    else:
-        mask_cube = None
-
-
-    if mask_sn_pix is not None:
-        mask *= mask_sn_pix
-
-    if mask_cube is not None:
-        mask *= mask_cube
-
-    if mask_sky is not None:
-        mask *= mask_sky
-
-    return mask
 
 def _auto_truncate_crop_cube(cube, params=None,
-            pixscale=None, spec_type='velocity', spec_arr=None,
+            spec_type='velocity', spec_arr=None,
             err_cube=None, mask_cube=None, mask_sky=None, mask_spec=None,
             spec_unit=u.km/u.s,weight=None, xcenter=None, ycenter=None):
 
+
+    cube_orig = cube.copy()
+
     # First truncate by spec:
     if 'spec_vel_trim' in params.keys():
-        whin = np.where((spec_arr >= params['spec_vel_trim'][0]) & (spec_arr <= params['spec_vel_trim'][1]))[0]
-        spec_arr = spec_arr[whin]
-        cube = cube[whin, :, :]
-        err_cube = err_cube[whin, :, :]
+        wh_spec_keep = np.where((spec_arr >= params['spec_vel_trim'][0]) & (spec_arr <= params['spec_vel_trim'][1]))[0]
+        spec_arr = spec_arr[wh_spec_keep]
+        cube = cube[wh_spec_keep, :, :]
+        err_cube = err_cube[wh_spec_keep, :, :]
 
         if mask_cube is not None:
-            mask_cube = mask_cube[whin, :, :]
+            mask_cube = mask_cube[wh_spec_keep, :, :]
         if mask_sky is not None:
-            mask_sky = mask_sky[whin, :, :]
+            mask_sky = mask_sky[wh_spec_keep, :, :]
         if mask_spec is not None:
-            mask_spec = mask_spec[whin, :, :]
+            mask_spec = mask_spec[wh_spec_keep, :, :]
         if weight is not None:
-            weight = weight[whin, :, :]
-
+            weight = weight[wh_spec_keep, :, :]
 
     # Then truncate area:
+    sp_trm = None
     if 'spatial_crop_trim' in params.keys():
         # left right bottom top
         sp_trm = np.array(params['spatial_crop_trim'], dtype=np.int32)
@@ -175,39 +238,79 @@ def _auto_truncate_crop_cube(cube, params=None,
 
 
     ##############
-    # Then check for first / last non-masked parts:
-    if mask_cube is not None:
-        mcube = cube.copy()*mask_cube.copy()
-    else:
-        mcube = cube.copy()
+    wh_ends_trim = None
+    if 'auto_trim_ends' in params.keys():
+        if params['auto_trim_ends']:
+            # Then check for first / last non-masked parts:
+            if mask_cube is not None:
+                mcube = cube.copy()*mask_cube.copy()
+            else:
+                mcube = cube.copy()
 
-    mcube[~np.isfinite(mcube)] = 0.
-    mcube=np.abs(mcube)
-    c_sum_spec = mcube.sum(axis=(1,2))
-    c_spec_up = np.cumsum(c_sum_spec)
-    c_spec_down = np.cumsum(c_sum_spec[::-1])
+            mcube[~np.isfinite(mcube)] = 0.
+            mcube=np.abs(mcube)
+            c_sum_spec = mcube.sum(axis=(1,2))
+            c_spec_up = np.cumsum(c_sum_spec)
+            c_spec_down = np.cumsum(c_sum_spec[::-1])
 
-    wh_l = np.where(c_spec_up > 0.)[0][0]
-    wh_r = np.where(c_spec_down > 0.)[0][0]
-    if (wh_l > 0) | (wh_r > 0):
-        if wh_r == 0:
-            v_wh_r = len(c_spec_down)
-        else:
-            v_wh_r = -wh_r
+            wh_l = np.where(c_spec_up > 0.)[0][0]
+            wh_r = np.where(c_spec_down > 0.)[0][0]
+            if (wh_l > 0) | (wh_r > 0):
+                if wh_r == 0:
+                    v_wh_r = len(c_spec_down)
+                else:
+                    v_wh_r = -wh_r
 
-        spec_arr = spec_arr[wh_l:v_wh_r]
-        cube = cube[wh_l:v_wh_r, :, :]
-        err_cube = err_cube[wh_l:v_wh_r, :, :]
+                wh_ends_trim = [wh_l, v_wh_r]
 
-        if mask_cube is not None:
-            mask_cube = mask_cube[wh_l:v_wh_r, :, :]
-        if mask_sky is not None:
-            mask_sky = mask_sky[wh_l:v_wh_r, :, :]
-        if mask_spec is not None:
-            mask_spec = mask_spec[wh_l:v_wh_r, :, :]
-        if weight is not None:
-            weight = weight[wh_l:v_wh_r, :, :]
+                spec_arr = spec_arr[wh_l:v_wh_r]
+                cube = cube[wh_l:v_wh_r, :, :]
+                err_cube = err_cube[wh_l:v_wh_r, :, :]
+
+                if mask_cube is not None:
+                    mask_cube = mask_cube[wh_l:v_wh_r, :, :]
+                if mask_sky is not None:
+                    mask_sky = mask_sky[wh_l:v_wh_r, :, :]
+                if mask_spec is not None:
+                    mask_spec = mask_spec[wh_l:v_wh_r, :, :]
+                if weight is not None:
+                    weight = weight[wh_l:v_wh_r, :, :]
+
 
 
     #####
-    return cube, err_cube, mask_cube, mask_sky, mask_spec, weight, spec_arr, xcenter, ycenter
+    return cube, err_cube, mask_cube, mask_sky, mask_spec, weight, \
+                    spec_arr, xcenter, ycenter, wh_spec_keep, wh_ends_trim, sp_trm
+
+
+
+def pad_3D_mask_to_uncropped_size(gal=None, mask=None):
+    spec_unit=u.Unit(gal.data.data.wcs.wcs.cunit[2].to_string())
+    spec_arr = gal.data.data.spectral_axis.to(spec_unit).value
+
+    if gal.data.cube_precrop_sh is not None:
+        mask_cube = np.zeros(gal.data.cube_precrop_sh)
+        sp_trm = gal.data.sp_trm
+        if sp_trm is None:
+            sp_trm = [0,mask_cube.shape[2],0,mask_cube.shape[1]]
+
+        if gal.data.wh_spec_keep is not None:
+            spc_trm = [gal.data.wh_spec_keep[0], gal.data.wh_spec_keep[-1]+1]
+        else:
+            spc_trm = [0, mask_cube.shape[0]]
+
+        if gal.data.wh_ends_trim is not None:
+            spc_trm[0] += gal.data.wh_ends_trim[0]
+            spc_trm[1] += gal.data.wh_ends_trim[0]
+            if gal.data.wh_ends_trim[1]< 0:
+                spc_trm[1] += gal.data.wh_ends_trim[1]
+
+        mask_cube[spc_trm[0]:spc_trm[1], sp_trm[2]:sp_trm[3], sp_trm[0]:sp_trm[1]] = mask.copy()
+
+        spec_step = gal.data.data.wcs.wcs.cdelt[2]
+        spec_start = gal.data.data.wcs.wcs.crval[2] - (spc_trm[0]+1-gal.data.data.wcs.wcs.crpix[2])
+        spec_arr = np.arange(gal.data.cube_precrop_sh[0]) * spec_step + spec_start
+
+    else:
+        mask_cube = mask.copy()
+    return mask_cube, spec_arr

dysmalpy/plotting.py
@@ -1815,6 +1815,7 @@ def plot_aperture_compare_3D_cubes(gal,
                 fileout=None,
                 slit_width=None, slit_pa=None,
                 aper_dist=None,
+                skip_fits=True,
                 overwrite=False):
 
     if datacube is None:
@@ -1879,39 +1880,39 @@ def plot_aperture_compare_3D_cubes(gal,
              nx=nx, ny=ny, center_pixel=center_pixel, pixscale=rstep,
              moment=False)
 
+    if not skip_fits:
+        data_scaled = datacube.unmasked_data[:].value
+        if errcube is not None:
+            ecube =  errcube.unmasked_data[:].value * mask
+        else:
+            ecube = None
+        model_scaled = modelcube.unmasked_data[:].value
 
-    data_scaled = datacube.unmasked_data[:].value
-    if errcube is not None:
-        ecube =  errcube.unmasked_data[:].value * mask
-    else:
-        ecube = None
-    model_scaled = modelcube.unmasked_data[:].value
+        aper_centers, flux1d, vel1d, disp1d = apertures.extract_1d_kinematics(spec_arr=specarr,
+                        cube=data_scaled, mask=mask, err=ecube,
+                        center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
 
-    aper_centers, flux1d, vel1d, disp1d = apertures.extract_1d_kinematics(spec_arr=specarr,
-                    cube=data_scaled, mask=mask, err=ecube,
-                    center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
+        ## UNWEIGHTED GAUS FIT TO DATA
+        # aper_centers2, flux1d2, vel1d2, disp1d2 = apertures.extract_1d_kinematics(spec_arr=specarr,
+        #                 cube=data_scaled, mask=mask, err=None,
+        #                 center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
 
-    ## UNWEIGHTED GAUS FIT TO DATA
-    # aper_centers2, flux1d2, vel1d2, disp1d2 = apertures.extract_1d_kinematics(spec_arr=specarr,
-    #                 cube=data_scaled, mask=mask, err=None,
-    #                 center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
+        #####
+        aper_centers_mod, flux1d_mod, vel1d_mod, disp1d_mod = apertures.extract_1d_kinematics(spec_arr=specarr,
+                        cube=model_scaled, mask=mask, err=None,
+                        center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
 
-    #####
-    aper_centers_mod, flux1d_mod, vel1d_mod, disp1d_mod = apertures.extract_1d_kinematics(spec_arr=specarr,
-                    cube=model_scaled, mask=mask, err=None,
-                    center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
+        apertures_mom = CircApertures(rarr=aper_centers_pix, slit_PA=slit_pa, rpix=rpix,
+                 nx=nx, ny=ny, center_pixel=center_pixel, pixscale=rstep,
+                 moment=True)
+        aper_centers_mod_mom, flux1d_mod_mom, vel1d_mod_mom, disp1d_mod_mom = apertures_mom.extract_1d_kinematics(spec_arr=specarr,
+                        cube=model_scaled, mask=mask, err=None,
+                        center_pixel = center_pixel,
+                        pixscale=gal.instrument.pixscale.value)
 
-    apertures_mom = CircApertures(rarr=aper_centers_pix, slit_PA=slit_pa, rpix=rpix,
-             nx=nx, ny=ny, center_pixel=center_pixel, pixscale=rstep,
-             moment=True)
-    aper_centers_mod_mom, flux1d_mod_mom, vel1d_mod_mom, disp1d_mod_mom = apertures_mom.extract_1d_kinematics(spec_arr=specarr,
-                    cube=model_scaled, mask=mask, err=None,
-                    center_pixel = center_pixel,
-                    pixscale=gal.instrument.pixscale.value)
-
-    aper_centers2, flux1d2, vel1d2, disp1d2 = apertures_mom.extract_1d_kinematics(spec_arr=specarr,
-                    cube=data_scaled, mask=mask, err=ecube,
-                    center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
+        aper_centers2, flux1d2, vel1d2, disp1d2 = apertures_mom.extract_1d_kinematics(spec_arr=specarr,
+                        cube=data_scaled, mask=mask, err=ecube,
+                        center_pixel = center_pixel, pixscale=gal.instrument.pixscale.value)
 
     ######################################
     # Setup plot:
@@ -1959,29 +1960,38 @@ def plot_aperture_compare_3D_cubes(gal,
                     cube=mask, skip_specmask=True)
             maskarr[maskarr>0] = 1.
 
+            if not skip_fits:
+                gmod_flux2=flux1d_mod_mom[k]
+                gmod_vel2=vel1d_mod_mom[k]
+                gmod_disp2=disp1d_mod_mom[k]
 
-            gmod_flux2=flux1d_mod_mom[k]
-            gmod_vel2=vel1d_mod_mom[k]
-            gmod_disp2=disp1d_mod_mom[k]
+                gmod_flux2 = gmod_vel2 = gmod_disp2 = None
 
-            gmod_flux2 = None
-            gmod_vel2 = None
-            gmod_disp2 = None
+                gdata_flux2=flux1d2[k]
+                gdata_vel2=vel1d2[k]
+                gdata_disp2=disp1d2[k]
+                # gdata_flux2=None
+                # gdata_vel2=None
+                # gdata_disp2=None
 
-            gdata_flux2=flux1d2[k]
-            gdata_vel2=vel1d2[k]
-            gdata_disp2=disp1d2[k]
-            # gdata_flux2=None
-            # gdata_vel2=None
-            # gdata_disp2=None
+                gdata_flux=flux1d[k]
+                gdata_vel=vel1d[k]
+                gdata_disp=disp1d[k]
+                gmod_flux=flux1d_mod[k]
+                gmod_vel=vel1d_mod[k]
+                gmod_disp=disp1d_mod[k]
+
+            else:
+                gdata_flux = gdata_vel = gdata_disp = None
+                gdata_flux2 = gdata_vel2 = gdata_disp2 = None
+                gmod_flux = gmod_vel = gmod_disp = None
+                gmod_flux2 = gmod_vel2 = gmod_disp2 = None
 
             ax = plot_spaxel_fit(specarr, datarr, maskarr, err=errarr,
-                gdata_flux=flux1d[k], gdata_vel=vel1d[k], gdata_disp=disp1d[k],
+                gdata_flux=gdata_flux, gdata_vel=gdata_vel, gdata_disp=gdata_disp,
                 gdata_flux2=gdata_flux2, gdata_vel2=gdata_vel2, gdata_disp2=gdata_disp2,
-                model=modarr, gmod_flux=flux1d_mod[k], gmod_vel=vel1d_mod[k],
-                gmod_disp=disp1d_mod[k],
-                gmod_flux2=gmod_flux2, gmod_vel2=gmod_vel2,
-                gmod_disp2=gmod_disp2,
+                model=modarr, gmod_flux=gmod_flux, gmod_vel=gmod_vel, gmod_disp=gmod_disp,
+                gmod_flux2=gmod_flux2, gmod_vel2=gmod_vel2, gmod_disp2=gmod_disp2,
                 ax=ax)
 
             ax.annotate('Ap {}'.format(k),
@@ -2011,7 +2021,8 @@ def plot_spaxel_compare_3D_cubes(gal,
                 fileout=None,
                 typ='all',
                 show_model=True,
-                skip_masked=True,
+                skip_masked=False,
+                skip_fits=True,
                 overwrite=False):
 
     if typ.strip().lower() not in ['all', 'diag']:
@@ -2027,7 +2038,11 @@ def plot_spaxel_compare_3D_cubes(gal,
         mask = np.array(gal.data.mask, dtype=np.float)
     if show_model:
         if modelcube is None:
-            modelcube = gal.model_data.data
+            try:
+                modelcube = gal.model_data.data
+            except:
+                # Skip showing model
+                show_model = False
 
     # Check for existing file:
     if (not overwrite) and (fileout is not None):
@@ -2041,9 +2056,7 @@ def plot_spaxel_compare_3D_cubes(gal,
     ny = datacube.shape[1]
     npix = np.max([nx,ny])
 
-
     specarr = datacube.spectral_axis.to(u.km/u.s).value
-
 
     ######################################
     # Setup plot:
@@ -2097,21 +2110,18 @@ def plot_spaxel_compare_3D_cubes(gal,
             axes.append(plt.subplot(gs[ii,j]))
 
 
-
     #############################################################
-    if show_model:
-        mom0_mod = modelcube.moment0().to(u.km/u.s).value
-        mom1_mod = modelcube.moment1().to(u.km/u.s).value
-        mom2_mod = modelcube.linewidth_sigma().to(u.km/u.s).value
+    if not skip_fits:
+        if show_model:
+            mom0_mod = modelcube.moment0().to(u.km/u.s).value
+            mom1_mod = modelcube.moment1().to(u.km/u.s).value
+            mom2_mod = modelcube.linewidth_sigma().to(u.km/u.s).value
 
-    maskbool = np.array(mask, dtype=np.bool)
-    datacube_masked = datacube.with_mask(maskbool)
-    mom0_dat = datacube_masked.moment0().to(u.km/u.s).value
-    mom1_dat = datacube_masked.moment1().to(u.km/u.s).value
-    mom2_dat = datacube_masked.linewidth_sigma().to(u.km/u.s).value
-
-
-    specstep = np.average(specarr[1:]-specarr[:-1])
+        maskbool = np.array(mask, dtype=np.bool)
+        datacube_masked = datacube.with_mask(maskbool)
+        mom0_dat = datacube_masked.moment0().to(u.km/u.s).value
+        mom1_dat = datacube_masked.moment1().to(u.km/u.s).value
+        mom2_dat = datacube_masked.linewidth_sigma().to(u.km/u.s).value
 
     # for each spax:
     k = -1
@@ -2143,56 +2153,63 @@ def plot_spaxel_compare_3D_cubes(gal,
                         do_plot = False
 
                 if ((typ == 'all') & (do_plot)) | (typ == 'diag'):
-                    if show_model:
-                        flux1d_mod_mom = mom0_mod[i,j]
-                        vel1d_mod_mom = mom1_mod[i,j]
-                        disp1d_mod_mom = mom2_mod[i,j]
+                    if not skip_fits:
+                        if show_model:
+                            flux1d_mod_mom = mom0_mod[i,j]
+                            vel1d_mod_mom = mom1_mod[i,j]
+                            disp1d_mod_mom = mom2_mod[i,j]
 
-                        best_fit = gaus_fit_sp_opt_leastsq(specarr, modarr, mom0_mod[i,j],
-                                        mom1_mod[i,j], mom2_mod[i,j])
-                        flux1d_mod = best_fit[0] * np.sqrt(2 * np.pi) * best_fit[2]
-                        vel1d_mod = best_fit[1]
-                        disp1d_mod = best_fit[2]
+                            best_fit = gaus_fit_sp_opt_leastsq(specarr, modarr, mom0_mod[i,j],
+                                            mom1_mod[i,j], mom2_mod[i,j])
+                            flux1d_mod = best_fit[0] * np.sqrt(2 * np.pi) * best_fit[2]
+                            vel1d_mod = best_fit[1]
+                            disp1d_mod = best_fit[2]
+                        else:
+                            flux1d_mod_mom = None
+                            vel1d_mod_mom = None
+                            disp1d_mod_mom = None
+                            flux1d_mod = None
+                            vel1d_mod = None
+                            disp1d_mod = None
+
+                        maskarr_bool = np.array(maskarr, dtype=np.bool)
+
+                        flux1d2 = mom0_dat[i,j]
+                        vel1d2 = mom1_dat[i,j]
+                        disp1d2 = mom2_dat[i,j]
+                        try:
+                            best_fit = gaus_fit_apy_mod_fitter(specarr[maskarr_bool], datarr[maskarr_bool],
+                                            mom0_dat[i,j], mom1_dat[i,j], mom2_dat[i,j], yerr=errarr[maskarr_bool])
+                        except:
+                            best_fit = [np.NaN, np.NaN, np.NaN]
+                        flux1d = best_fit[0] * np.sqrt(2 * np.pi) * best_fit[2]
+                        vel1d = best_fit[1]
+                        disp1d = best_fit[2]
+
+                        # UNWEIGHTED:
+                        # best_fit = gaus_fit_sp_opt_leastsq(specarr[maskarr_bool], datarr[maskarr_bool],
+                        #                 mom0[i,j], mom1[i,j], mom2[i,j])
+                        # flux1d2 = best_fit[0] * np.sqrt(2 * np.pi) * best_fit[2]
+                        # vel1d2 = best_fit[1]
+                        # disp1d2 = best_fit[2]
+                        # # flux1d2 = None
+                        # # vel1d2 = None
+                        # # disp1d2 = None
+
+                        gmod_flux2=flux1d_mod_mom
+                        gmod_vel2=vel1d_mod_mom
+                        gmod_disp2=disp1d_mod_mom
+
+                        # gmod_flux2 = None
+                        # gmod_vel2 = None
+                        # gmod_disp2 = None
+
                     else:
-                        flux1d_mod_mom = None
-                        vel1d_mod_mom = None
-                        disp1d_mod_mom = None
-                        flux1d_mod = None
-                        vel1d_mod = None
-                        disp1d_mod = None
+                        flux1d = vel1d = disp1d = None
+                        flux1d2 = vel1d2 = disp1d2 = None
+                        flux1d_mod = vel1d_mod = disp1d_mod = None
+                        gmod_flux2 = gmod_vel2 = gmod_disp2 = None
 
-                    maskarr_bool = np.array(maskarr, dtype=np.bool)
-
-                    flux1d2 = mom0_dat[i,j]
-                    vel1d2 = mom1_dat[i,j]
-                    disp1d2 = mom2_dat[i,j]
-                    try:
-                        best_fit = gaus_fit_apy_mod_fitter(specarr[maskarr_bool], datarr[maskarr_bool],
-                                        mom0_dat[i,j], mom1_dat[i,j], mom2_dat[i,j], yerr=errarr[maskarr_bool])
-                    except:
-                        best_fit = [np.NaN, np.NaN, np.NaN]
-                    flux1d = best_fit[0] * np.sqrt(2 * np.pi) * best_fit[2]
-                    vel1d = best_fit[1]
-                    disp1d = best_fit[2]
-
-
-                    # UNWEIGHTED:
-                    # best_fit = gaus_fit_sp_opt_leastsq(specarr[maskarr_bool], datarr[maskarr_bool],
-                    #                 mom0[i,j], mom1[i,j], mom2[i,j])
-                    # flux1d2 = best_fit[0] * np.sqrt(2 * np.pi) * best_fit[2]
-                    # vel1d2 = best_fit[1]
-                    # disp1d2 = best_fit[2]
-                    # # flux1d2 = None
-                    # # vel1d2 = None
-                    # # disp1d2 = None
-
-                    gmod_flux2=flux1d_mod_mom
-                    gmod_vel2=vel1d_mod_mom
-                    gmod_disp2=disp1d_mod_mom
-
-                    # gmod_flux2 = None
-                    # gmod_vel2 = None
-                    # gmod_disp2 = None
 
                     ax = plot_spaxel_fit(specarr, datarr, maskarr, err=errarr,
                         gdata_flux=flux1d, gdata_vel=vel1d, gdata_disp=disp1d,
@@ -2206,6 +2223,9 @@ def plot_spaxel_compare_3D_cubes(gal,
                     ax.annotate('Pix ({},{})'.format(j,i),
                             (0.02,0.98), xycoords='axes fraction',
                             ha='left', va='top', fontsize=8)
+
+                    if (maskarr.max() <= 0):
+                        ax.set_facecolor('#f0f0f0') #'#e3e3e3')
                 else:
                     ax.set_axis_off()
             elif (k < len(axes)) & (not skip) & (k >= npix):
@@ -2431,37 +2451,49 @@ def plot_spaxel_fit(specarr, data, mask, err=None,
         returnax = False
         ax = plt.subplot(111)
 
-    ax.plot(specarr, data, color='black', marker='o', ms=4., mfc='None', ls='None', alpha=0.5)
-    ax.plot(specarr, data*mask, color='black', marker='o', ms=4.)
 
-    gdata_A = gdata_flux / ( np.sqrt(2 * np.pi) * gdata_disp)
-    ax.plot(specarr, gdata_A*np.exp(-((specarr-gdata_vel)**2/(2.*gdata_disp**2))),
-            color='turquoise',zorder=10., lw=0.5)
+    ax.plot(specarr, data*mask, color='black', marker='o', ms=4., zorder=1.)
+    if (mask.max() > 0):
+        ylim = ax.get_ylim()
+    ax.plot(specarr, data, color='black', marker='o', ms=4., mfc='None', ls='None', alpha=0.5, zorder=0.)
+    if (mask.max() > 0):
+        ax.set_ylim(ylim)
 
-
-    try:
-        gdata_A2 = gdata_flux2 / ( np.sqrt(2 * np.pi) * gdata_disp2)
-        ax.plot(specarr, gdata_A2*np.exp(-((specarr-gdata_vel2)**2/(2.*gdata_disp2**2))),
-                color='tab:green', ls='--', zorder=5., lw=0.5)
-    except:
-        pass
-
-    if model is not None:
-        ax.plot(specarr, model, color='red', ls='-', lw=1., alpha=0.5)
-        ax.plot(specarr, model*mask, color='red',lw=1.5)
-
-
-        gmod_A = gmod_flux / ( np.sqrt(2 * np.pi) * gmod_disp)
-        ax.plot(specarr, gmod_A*np.exp(-((specarr-gmod_vel)**2/(2.*gmod_disp**2))),
-                    color='orange', ls='--', zorder=10., lw=0.5)
-
+    if gdata_flux is not None:
         try:
-            gmod_A2 = gmod_flux2 / ( np.sqrt(2 * np.pi) * gmod_disp2)
-            ax.plot(specarr, gmod_A2*np.exp(-((specarr-gmod_vel2)**2/(2.*gmod_disp2**2))),
-                    color='purple', ls=':', zorder=10., lw=0.5)
+            gdata_A = gdata_flux / ( np.sqrt(2 * np.pi) * gdata_disp)
+            ax.plot(specarr, gdata_A*np.exp(-((specarr-gdata_vel)**2/(2.*gdata_disp**2))),
+                    color='turquoise',zorder=10., lw=0.5)
         except:
             pass
 
+    if gdata_flux2 is not None:
+        try:
+            gdata_A2 = gdata_flux2 / ( np.sqrt(2 * np.pi) * gdata_disp2)
+            ax.plot(specarr, gdata_A2*np.exp(-((specarr-gdata_vel2)**2/(2.*gdata_disp2**2))),
+                    color='tab:green', ls='--', zorder=5., lw=0.5)
+        except:
+            pass
+
+    if model is not None:
+        ax.plot(specarr, model, color='red', ls='-', lw=1., alpha=0.5, zorder=1.)
+        ax.plot(specarr, model*mask, color='red',lw=1.5, zorder=1.)
+
+        if gmod_flux is not None:
+            try:
+                gmod_A = gmod_flux / ( np.sqrt(2 * np.pi) * gmod_disp)
+                ax.plot(specarr, gmod_A*np.exp(-((specarr-gmod_vel)**2/(2.*gmod_disp**2))),
+                            color='orange', ls='--', zorder=10., lw=0.5)
+            except:
+                pass
+
+        if gmod_flux2 is not None:
+            try:
+                gmod_A2 = gmod_flux2 / ( np.sqrt(2 * np.pi) * gmod_disp2)
+                ax.plot(specarr, gmod_A2*np.exp(-((specarr-gmod_vel2)**2/(2.*gmod_disp2**2))),
+                        color='purple', ls=':', zorder=10., lw=0.5)
+            except:
+                pass
 
     if err is not None:
         ylim = ax.get_ylim()
@@ -2498,7 +2530,6 @@ def plot_spaxel_fit(specarr, data, mask, err=None,
     if xmajloc is not None:
         ax.xaxis.set_major_locator(MultipleLocator(xmajloc))
         ax.xaxis.set_minor_locator(MultipleLocator(xminloc))
-
 
     if returnax:
         return ax