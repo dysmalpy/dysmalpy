ModelSet.circular_velocity fix for neg potl gradients
Fixed circular_velocity for inclusion of components with negative potential gradients (eg, massive gaussian rings).

dysmalpy/models/baryons.py
@@ -775,6 +775,7 @@ class InfThinMassiveGaussianRing(object):
 
     def _reset_interps(self):
         self._set_vcirc_interp()
+        self._set_potential_gradient_interp()
         self._set_menc_interp()
 
 
@@ -806,9 +807,7 @@ class InfThinMassiveGaussianRing(object):
 
         return t[0]
 
-
     def _set_vcirc_interp(self):
-        # SHOULD BE EXACTLY, w/in numerical limitations, EQUIV TO OLD CALCULATION
         table = self.read_ring_table()
 
         tab_rad =               table['R']
@@ -824,6 +823,22 @@ class InfThinMassiveGaussianRing(object):
         # scale_fac = np.sqrt(total_mass / table_mass) * np.sqrt(table_Rpeak / R_peak)
         # vcirc_interp = v_interp(Rarr / R_peak * table_Rpeak) * scale_fac
 
+
+    def _set_potential_gradient_interp(self):
+        table = self.read_ring_table()
+
+        tab_rad =               table['R']
+        tab_potl_grad =         table['potential_gradient']
+        self.tab_invh =         table['invh']
+        self.tab_R_peak =       table['R_peak']
+        self.tab_ring_FWHM =    table['ring_FWHM']
+        self.tab_mass =         table['total_mass']
+
+        self.potl_grad_interp = scp_interp.interp1d(tab_rad, tab_potl_grad,
+                                       fill_value="extrapolate")
+
+        # scale_fac = (total_mass / table_mass) * (table_Rpeak / R_peak)**2
+        # potential_gradient_interp = potl_grad_interp(Rarr / R_peak * table_Rpeak) * scale_fac
 
     def _set_menc_interp(self):
         # SHOULD BE EXACTLY, w/in numerical limitations, EQUIV TO OLD CALCULATION
@@ -879,6 +894,38 @@ class InfThinMassiveGaussianRing(object):
         return vcirc
 
 
+    def potential_gradient(self, R, R_peak, total_mass):
+        """
+        Calculate potential gradient for a inf thin massive gaussian Ring
+
+        Parameters
+        ----------
+        R : float or array
+            Radius or radii at which to calculate the potential gradient in kpc
+
+        R_peak : float
+            Peak of Gaussian ring in kpc
+
+        total_mass : float
+            Total mass of the Gaussian ring component
+
+        Returns
+        -------
+        potl_grad : float or array
+            Potential gradient at each given `R`
+
+        Notes
+        -----
+        This function determines the potential gradient as a function of radius for
+        a massive Gaussian ring component with a total mass, `total_mass`,
+        and a ring peak radius to ring FWHM ratio, `invh`.
+        This uses numerically calculated lookup tables.
+
+        """
+        scale_fac = total_mass / self.tab_mass * (self.tab_R_peak / R_peak)**2
+        potential_gradient_interp = self.potl_grad_interp(R / R_peak * self.tab_R_peak) * scale_fac
+
+        return potential_gradient_interp
 
     def enclosed_mass(self, R, R_peak, total_mass):
         """
@@ -2527,6 +2574,7 @@ class GaussianRing(MassModel, _LightMassModel):
     mass_to_light = DysmalParameter(default=1, fixed=True)
 
     _subtype = 'baryonic'
+    _potential_gradient_has_neg = True
 
     def __init__(self, baryon_type='gas+stars', **kwargs):
 
@@ -2619,6 +2667,34 @@ class GaussianRing(MassModel, _LightMassModel):
 
         return self.ring_table.vcirc(r, self.R_peak.value, 10**self.total_mass.value)
 
+    def potential_gradient(self, r):
+        r"""
+        Default method to evaluate the gradient of the potential, :math:`\del\Phi(r)/\del r`.
+
+        Parameters
+        ----------
+        r : float or array
+            Radius or radii at which to calculate circular velocity in kpc
+
+        Returns
+        -------
+        dPhidr : float or array
+            Gradient of the potential at `r`
+
+        Notes
+        -----
+        Calculates the gradient of the potential from the circular velocity
+        using :math:`\del\Phi(r)/\del r = v_{c}^2(r)/r`.
+        An alternative should be written for components where the
+        potential gradient is ever *negative* (i.e., rings).
+
+        Can be coupled with setting & checking `model._potential_gradient_has_neg` flag
+        for mass models.
+        """
+        # Check invh is correct
+        self._update_ring_table()
+
+        return self.ring_table.potential_gradient(r, self.R_peak.value, 10**self.total_mass.value)
 
     def light_profile(self, r):
         """
@@ -2635,7 +2711,7 @@ class GaussianRing(MassModel, _LightMassModel):
             Relative line flux as a function of radius
         """
         #return 1.*np.exp(-(r-self.R_peak.value)**2/(2.*self.sigma_R()**2))
-        return self.surface_density(r) * (1./self.mass_to_light) 
+        return self.surface_density(r) * (1./self.mass_to_light)
 
     def rhogas(self, r):
         """

dysmalpy/models/kinematic_options.py
@@ -116,8 +116,155 @@ class KinematicOptions:
         self.pressure_support_n = pressure_support_n
         self.pressure_support_type = pressure_support_type
 
+    
+    # def apply_adiabatic_contract(self, model, r, vbaryon, vhalo,
+    #                              compute_dm=False,
+    #                              model_key_re=['disk+bulge', 'r_eff_disk'],
+    #                              step1d = 0.2):
+    #     """
+    #     Function that applies adiabatic contraction to a ModelSet
+    #
+    #     Parameters
+    #     ----------
+    #     model : `ModelSet`
+    #         ModelSet that adiabatic contraction will be applied to
+    #
+    #     r : array
+    #         Radii in kpc
+    #
+    #     vbaryon : array
+    #         Baryonic component circular velocities in km/s
+    #
+    #     vhalo : array
+    #         Dark matter halo circular velocities in km/s
+    #
+    #     compute_dm : bool
+    #         If True, will return the adiabatically contracted halo velocities.
+    #
+    #     model_key_re : list
+    #         Two element list which contains the name of the model component
+    #         and parameter to use for the effective radius.
+    #         Default is ['disk+bulge', 'r_eff_disk'].
+    #
+    #     step1d : float
+    #         Step size in kpc to use during adiabatic contraction calculation
+    #
+    #     Returns
+    #     -------
+    #     vel : array
+    #        Total circular velocity corrected for adiabatic contraction in km/s
+    #
+    #     vhalo_adi : array
+    #         Dark matter halo circular velocities corrected for adiabatic contraction.
+    #         Only returned if `compute_dm` = True
+    #     """
+    #
+    #     if self.adiabatic_contract:
+    #         #logger.info("Applying adiabatic contraction.")
+    #
+    #         # Define 1d radius array for calculation
+    #         #step1d = 0.2  # kpc
+    #         # r1d = np.arange(step1d, np.ceil(r.max()/step1d)*step1d+ step1d, step1d, dtype=np.float64)
+    #         try:
+    #             rmaxin = r.max()
+    #         except:
+    #             rmaxin = r
+    #         # Get reff:
+    #         comp = model.components.__getitem__(model_key_re[0])
+    #         param_i = comp.param_names.index(model_key_re[1])
+    #         r_eff = comp.parameters[param_i]
+    #
+    #         rmax_calc = max(5.* r_eff, rmaxin)
+    #
+    #         # Wide enough radius range for full calculation -- out to 5*Reff, at least
+    #         r1d = np.arange(step1d, np.ceil(rmax_calc/step1d)*step1d+ step1d, step1d, dtype=np.float64)
+    #
+    #
+    #         rprime_all_1d = np.zeros(len(r1d))
+    #
+    #         # Calculate vhalo, vbaryon on this 1D radius array [note r is a 3D array]
+    #         vhalo1d = r1d * 0.
+    #         vbaryon1d = r1d * 0.
+    #         for cmp in model.mass_components:
+    #
+    #             if model.mass_components[cmp]:
+    #                 mcomp = model.components[cmp]
+    #                 if isinstance(mcomp, DiskBulge) | isinstance(mcomp, LinearDiskBulge):
+    #                     cmpnt_v = mcomp.circular_velocity(r1d)
+    #                 else:
+    #                     cmpnt_v = mcomp.circular_velocity(r1d)
+    #                 if mcomp._subtype == 'dark_matter':
+    #
+    #                     vhalo1d = np.sqrt(vhalo1d ** 2 + cmpnt_v ** 2)
+    #
+    #                 elif mcomp._subtype == 'baryonic':
+    #
+    #                     vbaryon1d = np.sqrt(vbaryon1d ** 2 + cmpnt_v ** 2)
+    #
+    #                 elif mcomp._subtype == 'combined':
+    #
+    #                     raise ValueError('Adiabatic contraction cannot be turned on when'
+    #                                      'using a combined baryonic and halo mass model!')
+    #
+    #                 else:
+    #                     raise TypeError("{} mass model subtype not recognized"
+    #                                     " for {} component. Only 'dark_matter'"
+    #                                     " or 'baryonic' accepted.".format(mcomp._subtype, cmp))
+    #
+    #
+    #         converged = np.zeros(len(r1d), dtype=bool)
+    #         for i in range(len(r1d)):
+    #             try:
+    #                 result = scp_opt.newton(_adiabatic, r1d[i] + 1.,
+    #                                     args=(r1d[i], vhalo1d, r1d, vbaryon1d[i]),
+    #                                     maxiter=200)
+    #                 converged[i] = True
+    #             except:
+    #                 result = r1d[i]
+    #                 converged[i] = False
+    #
+    #             # ------------------------------------------------------------------
+    #             # HACK TO FIX WEIRD AC: If too weird: toss it...
+    #             if ('adiabatic_contract_modify_small_values' in self.__dict__.keys()):
+    #                 if self.adiabatic_contract_modify_small_values:
+    #                     if ((result < 0.) | (result > 5*max(r1d))):
+    #                         #print("tossing, mvir={}".format(model.components['halotmp'].mvirial.value))
+    #                         result = r1d[i]
+    #                         converged[i] = False
+    #             # ------------------------------------------------------------------
+    #
+    #             rprime_all_1d[i] = result
+    #
+    #
+    #         vhalo_adi_interp_1d = scp_interp.interp1d(r1d, vhalo1d, fill_value='extrapolate', kind='linear')   # linear interpolation
+    #
+    #         # Just calculations:
+    #         if converged.sum() < len(r1d):
+    #             if converged.sum() >= 0.9 *len(r1d):
+    #                 rprime_all_1d = rprime_all_1d[converged]
+    #                 r1d = r1d[converged]
+    #
+    #         vhalo_adi_1d = vhalo_adi_interp_1d(rprime_all_1d)
+    #
+    #         vhalo_adi_interp_map_3d = scp_interp.interp1d(r1d, vhalo_adi_1d, fill_value='extrapolate', kind='linear')
+    #
+    #         vhalo_adi = vhalo_adi_interp_map_3d(r)
+    #
+    #         vel = np.sqrt(vhalo_adi ** 2 + vbaryon ** 2)
+    #
+    #     else:
+    #         vel = np.sqrt(vhalo ** 2 + vbaryon ** 2)
+    #
+    #     if compute_dm:
+    #         if self.adiabatic_contract:
+    #             return vel, vhalo_adi
+    #         else:
+    #             return vel, vhalo
+    #     else:
+    #         return vel
 
-    def apply_adiabatic_contract(self, model, r, vbaryon, vhalo,
+
+    def apply_adiabatic_contract(self, model, r, vbaryon_sq, vhalo_sq,
                                  compute_dm=False,
                                  model_key_re=['disk+bulge', 'r_eff_disk'],
                                  step1d = 0.2):
@@ -132,11 +279,11 @@ class KinematicOptions:
         r : array
             Radii in kpc
 
-        vbaryon : array
-            Baryonic component circular velocities in km/s
+        vbaryon_sq : array
+            Square of baryonic component circular velocities in km^2/s^2
 
-        vhalo : array
-            Dark matter halo circular velocities in km/s
+        vhalo_sq : array
+            Square of dark matter halo circular velocities in km^2/s^2
 
         compute_dm : bool
             If True, will return the adiabatically contracted halo velocities.
@@ -183,26 +330,21 @@ class KinematicOptions:
             rprime_all_1d = np.zeros(len(r1d))
 
             # Calculate vhalo, vbaryon on this 1D radius array [note r is a 3D array]
-            vhalo1d = r1d * 0.
-            vbaryon1d = r1d * 0.
+            vhalo1d_sq = r1d * 0.
+            vbaryon1d_sq = r1d * 0.
             for cmp in model.mass_components:
-
                 if model.mass_components[cmp]:
                     mcomp = model.components[cmp]
-                    if isinstance(mcomp, DiskBulge) | isinstance(mcomp, LinearDiskBulge):
-                        cmpnt_v = mcomp.circular_velocity(r1d)
+                    if mcomp._potential_gradient_has_neg:
+                        cmpnt_v_sq = r * mcomp.potential_gradient(r1d)
                     else:
-                        cmpnt_v = mcomp.circular_velocity(r1d)
+                        cmpnt_v_sq = mcomp.circular_velocity(r1d) **2
+
                     if mcomp._subtype == 'dark_matter':
-
-                        vhalo1d = np.sqrt(vhalo1d ** 2 + cmpnt_v ** 2)
-
+                        vhalo1d_sq = vhalo1d_sq + cmpnt_v_sq
                     elif mcomp._subtype == 'baryonic':
-
-                        vbaryon1d = np.sqrt(vbaryon1d ** 2 + cmpnt_v ** 2)
-
+                        vbaryon1d_sq = vbaryon1d_sq + cmpnt_v_sq
                     elif mcomp._subtype == 'combined':
-
                         raise ValueError('Adiabatic contraction cannot be turned on when'
                                          'using a combined baryonic and halo mass model!')
 
@@ -210,7 +352,9 @@ class KinematicOptions:
                         raise TypeError("{} mass model subtype not recognized"
                                         " for {} component. Only 'dark_matter'"
                                         " or 'baryonic' accepted.".format(mcomp._subtype, cmp))
-
+            #vbaryon = np.sqrt(vbaryon_sq)
+            vhalo1d = np.sqrt(vhalo1d_sq)
+            vbaryon1d = np.sqrt(vbaryon1d_sq)
 
             converged = np.zeros(len(r1d), dtype=bool)
             for i in range(len(r1d)):
@@ -250,10 +394,14 @@ class KinematicOptions:
 
             vhalo_adi = vhalo_adi_interp_map_3d(r)
 
-            vel = np.sqrt(vhalo_adi ** 2 + vbaryon ** 2)
+
+            #vel = np.sqrt(vhalo_adi ** 2 + vbaryon ** 2)
+            vel = np.sqrt(vhalo_adi ** 2 + vbaryon_sq)
 
         else:
-            vel = np.sqrt(vhalo ** 2 + vbaryon ** 2)
+            vel = np.sqrt(vhalo_sq + vbaryon_sq)
+            if compute_dm:
+                vhalo = np.sqrt(vhalo_sq)
 
         if compute_dm:
             if self.adiabatic_contract:
@@ -262,6 +410,7 @@ class KinematicOptions:
                 return vel, vhalo
         else:
             return vel
+
 
     def apply_pressure_support(self, r, model, vel):
         """
@@ -451,5 +600,17 @@ def _adiabatic(rprime, r_adi, adia_v_dm, adia_x_dm, adia_v_disk):
                                         fill_value="extrapolate")
     result = (r_adi + r_adi * ((r_adi*adia_v_disk**2) /
                                (rprime*(rprime_interp(rprime))**2)) - rprime)
+
+    return result
+
+def _adiabatic_sq(rprime, r_adi, adia_v_dm_sq, adia_x_dm, adia_v_disk_sq):
+    if rprime <= 0.:
+        rprime = 0.1
+    if rprime < adia_x_dm[1]:
+        rprime = adia_x_dm[1]
+    rprime_interp = scp_interp.interp1d(adia_x_dm, adia_v_dm_sq,
+                                        fill_value="extrapolate")
+    result = (r_adi + r_adi * ((r_adi*adia_v_disk_sq) /
+                               (rprime*(rprime_interp(rprime)))) - rprime)
 
     return result

dysmalpy/models/model_set.py
@@ -1112,37 +1112,49 @@ class ModelSet:
             raise AttributeError("There are no mass components so a velocity "
                                  "can't be calculated.")
         else:
-            vdm = r*0.
-            vbaryon = r*0.
+            # vdm = r*0.
+            # vbaryon = r*0.
+            vdm_sq = r*0.
+            vbaryon_sq = r*0.
 
             for cmp in self.mass_components:
 
                 if self.mass_components[cmp]:
                     mcomp = self.components[cmp]
 
-                    # if isinstance(mcomp, DiskBulge) | isinstance(mcomp, LinearDiskBulge):
-                    #     cmpnt_v = mcomp.circular_velocity(r)
-                    # else:
-                    #     cmpnt_v = mcomp.circular_velocity(r)
+                    if mcomp._potential_gradient_has_neg:
+                        cmpnt_v_sq = r * mcomp.potential_gradient(r)
+                    else:
+                        cmpnt_v_sq = mcomp.circular_velocity(r) **2
 
-                    cmpnt_v = mcomp.circular_velocity(r)
                     if (mcomp._subtype == 'dark_matter') | (mcomp._subtype == 'combined'):
-
-                        vdm = np.sqrt(vdm ** 2 + cmpnt_v ** 2)
+                        #vdm = np.sqrt(vdm ** 2 + cmpnt_v ** 2)
+                        vdm_sq = vdm_sq + cmpnt_v_sq
 
                     elif mcomp._subtype == 'baryonic':
-
-                        vbaryon = np.sqrt(vbaryon ** 2 + cmpnt_v ** 2)
+                        #vbaryon = np.sqrt(vbaryon ** 2 + cmpnt_v ** 2)
+                        vbaryon_sq = vbaryon_sq + cmpnt_v_sq
 
                     else:
                         raise TypeError("{} mass model subtype not recognized"
                                         " for {} component. Only 'dark_matter'"
                                         " or 'baryonic' accepted.".format(
                                         mcomp._subtype, cmp))
-            vels = self.kinematic_options.apply_adiabatic_contract(self, r, vbaryon, vdm,
+
+            # vbaryon = np.sqrt(vbaryon_sq)
+            # vdm = np.sqrt(vdm_sq)
+            # vels = self.kinematic_options.apply_adiabatic_contract(self, r, vbaryon, vdm,
+            #                                                        compute_dm=compute_dm,
+            #                                                        model_key_re=model_key_re,
+            #                                                        step1d=step1d)
+
+            vels = self.kinematic_options.apply_adiabatic_contract(self, r, vbaryon_sq, vdm_sq,
                                                                    compute_dm=compute_dm,
                                                                    model_key_re=model_key_re,
                                                                    step1d=step1d)
+
+            if compute_baryon:
+                vbaryon = np.sqrt(vbaryon_sq)
 
             if compute_dm:
                 vel = vels[0]