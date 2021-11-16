Conditional priors, negative potential gradient fixes
- Add `parameters.ConditionalUniformPrior` -- the general prior class that has a flat prior within conditional boundaries, determined based on a function that specifies the boundaries
- Add `parameters.ConditionalEmpiricalUniformPrior` -- the general prior class that has a flat prior within conditional boundaries -- based on an empirical conditional function (and then the boundary function is determined iteratively using this function).
- Pass model set to Parameter.sample_prior(), for consistency when using conditional priors that need to access the full model set.
- Making a generic mass conditional bounds function... currently in baryons.py
- Bugfix in handling negative potential gradients for ring.

dysmalpy/fitting/mcmc.py
@@ -500,7 +500,7 @@ def _fit_emcee_221(gal, **kwargs ):
     start = time.time()
 
     if sampler.chain.shape[1] > 0:
-        logger.info('\n   Resuming with existing sampler chain at iteration ' + 
+        logger.info('\n   Resuming with existing sampler chain at iteration ' +
                     str(sampler.iteration) + '\n')
         pos = sampler['chain'][:,-1,:]
 
@@ -969,7 +969,7 @@ def _fit_emcee_3(gal, **kwargs ):
     start = time.time()
 
     if sampler.iteration > 0:
-        logger.info('\n   Resuming with existing sampler chain at iteration ' + 
+        logger.info('\n   Resuming with existing sampler chain at iteration ' +
                     str(sampler.iteration) + '\n')
         pos = sampler.get_last_sample()
 
@@ -1892,7 +1892,8 @@ def initialize_walkers(model, nWalkers=None):
         for paramn in params_names:
             if (pfree_dict[compn][paramn] >= 0) :
                 # Free parameter: randomly sample from prior nWalker times:
-                param_rand = comp.__getattribute__(paramn).prior.sample_prior(comp.__getattribute__(paramn), N=nWalkers)
+                param_rand = comp.__getattribute__(paramn).prior.sample_prior(comp.__getattribute__(paramn),
+                                    modelset=model, N=nWalkers)
                 stack_rand.append(param_rand)
     pos = np.array(list(zip(*stack_rand)))        # should have shape:   (nWalkers, nDim)
     return pos

dysmalpy/models/baryons.py
@@ -28,8 +28,9 @@ from dysmalpy.parameters import DysmalParameter
 
 __all__ = ['Sersic', 'DiskBulge', 'LinearDiskBulge', 'ExpDisk', 'BlackHole',
            'GaussianRing',
-           'surf_dens_exp_disk', 'menc_exp_disk', 'vcirc_exp_disk',
-           'sersic_menc_2D_proj', 'NoordFlat', 'InfThinMassiveGaussianRing']
+           'surf_dens_exp_disk', 'menc_exp_disk', 'vcirc_exp_disk', 'sersic_menc_2D_proj',
+           'mass_comp_conditional_ring',
+           'NoordFlat', 'InfThinMassiveGaussianRing']
 
 # NOORDERMEER DIRECTORY
 path = os.path.abspath(__file__)
@@ -41,10 +42,9 @@ _dir_noordermeer = os.sep.join([dir_path, "data", "noordermeer", ""])
 
 
 # ALT NOORDERMEER DIRECTORY:
-# TEMP:
-#_dir_sersic_profile_mass_VC_TMP = "/Users/sedona/data/sersic_profile_mass_VC/"
-_dir_sersic_profile_mass_VC_TMP = os.sep.join(["", "Users", "sedona", "data", "sersic_profile_mass_VC", ""])
-_dir_sersic_profile_mass_VC = os.getenv('SERSIC_PROFILE_MASS_VC_DATADIR', _dir_sersic_profile_mass_VC_TMP)
+# _dir_sersic_profile_mass_VC_TMP = os.sep.join(["", "Users", "sedona", "data", "sersic_profile_mass_VC", ""])
+#_dir_sersic_profile_mass_VC = os.getenv('SERSIC_PROFILE_MASS_VC_DATADIR', _dir_sersic_profile_mass_VC_TMP)
+_dir_sersic_profile_mass_VC = os.getenv('SERSIC_PROFILE_MASS_VC_DATADIR', None)
 
 # try:
 #     import sersic_profile_mass_VC.calcs as sersic_profile_mass_VC_calcs
@@ -53,11 +53,10 @@ _dir_sersic_profile_mass_VC = os.getenv('SERSIC_PROFILE_MASS_VC_DATADIR', _dir_s
 #     _sersic_profile_mass_VC_loaded = False
 
 # MASSIVE RING DIRECTORIES:
-#_dir_gaussian_ring_tables_TMP = "/Users/sedona/data/mpe_ir/ring_rot_curves/ring_RC_tables/"
-_dir_gaussian_ring_tables_TMP = os.sep.join(["", "Users", "sedona", "data",
-                                             "mpe_ir", "ring_rot_curves", "ring_RC_tables"])
-_dir_gaussian_ring_tables = os.getenv('GAUSSIAN_RING_PROFILE_DATADIR', _dir_gaussian_ring_tables_TMP)
-
+# _dir_gaussian_ring_tables_TMP = os.sep.join(["", "Users", "sedona", "data",
+#                                              "mpe_ir", "ring_rot_curves", "ring_RC_tables"])
+# _dir_gaussian_ring_tables = os.getenv('GAUSSIAN_RING_PROFILE_DATADIR', _dir_gaussian_ring_tables_TMP)
+_dir_gaussian_ring_tables = os.getenv('GAUSSIAN_RING_PROFILE_DATADIR', None)
 
 # CONSTANTS
 G = apy_con.G
@@ -202,6 +201,27 @@ def sersic_menc_2D_proj(r, mass, n, r_eff):
     menc = norm*integ
 
     return menc
+
+
+def mass_comp_conditional_ring(param, modelset):
+    """
+    Basic conditional prior on the mass of the other component(s) (i.e., a bulge or halo)
+    when fitting both a massive ring and one or more other mass components, returning True/False
+
+    This conditional could apply to the bulge mass, or to the halo mass (probably best to not do both).
+
+    Intended to be passed as f_bounds function when setting, e.g.,
+        sersic_comp.total_mass.prior = ConditionalEmpiricalUniformPrior(f_cond=mass_comp_conditional_bounds_ring)
+    """
+    # Double check param + model values are same:
+    if param.value != modelset.components[param._model._name].__getattribute__(param._name).value:
+        raise ValueError
+
+    # Test radius array:
+    rarr = np.arange(0., 10.1, 0.1)
+
+    return np.all(np.isfinite(modelset.circular_velocity(rarr)))
+
 
 
 ##########################
@@ -778,7 +798,7 @@ class InfThinMassiveGaussianRing(object):
         self._reset_interps()
 
     def _reset_interps(self):
-        self._set_vcirc_interp()
+        # self._set_vcirc_interp()
         self._set_potential_gradient_interp()
         self._set_menc_interp()
 
@@ -811,21 +831,21 @@ class InfThinMassiveGaussianRing(object):
 
         return t[0]
 
-    def _set_vcirc_interp(self):
-        table = self.read_ring_table()
-
-        tab_rad =               table['R']
-        tab_vcirc =             table['vcirc']
-        self.tab_invh =         table['invh']
-        self.tab_R_peak =       table['R_peak']
-        self.tab_ring_FWHM =    table['ring_FWHM']
-        self.tab_mass =         table['total_mass']
-
-        self.vcirc_interp = scp_interp.interp1d(tab_rad, tab_vcirc,
-                                       fill_value="extrapolate")
-
-        # scale_fac = np.sqrt(total_mass / table_mass) * np.sqrt(table_Rpeak / R_peak)
-        # vcirc_interp = v_interp(Rarr / R_peak * table_Rpeak) * scale_fac
+    # def _set_vcirc_interp(self):
+    #     table = self.read_ring_table()
+    #
+    #     tab_rad =               table['R']
+    #     tab_vcirc =             table['vcirc']
+    #     self.tab_invh =         table['invh']
+    #     self.tab_R_peak =       table['R_peak']
+    #     self.tab_ring_FWHM =    table['ring_FWHM']
+    #     self.tab_mass =         table['total_mass']
+    #
+    #     self.vcirc_interp = scp_interp.interp1d(tab_rad, tab_vcirc,
+    #                                    fill_value="extrapolate")
+    #
+    #     # scale_fac = np.sqrt(total_mass / table_mass) * np.sqrt(table_Rpeak / R_peak)
+    #     # vcirc_interp = v_interp(Rarr / R_peak * table_Rpeak) * scale_fac
 
 
     def _set_potential_gradient_interp(self):
@@ -860,42 +880,41 @@ class InfThinMassiveGaussianRing(object):
         # scale_fac = (total_mass / table_mass)
         # menc_interp = m_interp(Rarr / R_peak * table_Rpeak) * scale_fac
 
-
-    def vcirc(self, R, R_peak, total_mass):
-        """
-        Calculate circular velocity for a inf thin massive gaussian Ring
-
-        Parameters
-        ----------
-        R : float or array
-            Radius or radii at which to calculate the circular velocity in kpc
-
-        R_peak : float
-            Peak of Gaussian ring in kpc
-
-        total_mass : float
-            Total mass of the Gaussian ring component
-
-        Returns
-        -------
-        vcirc : float or array
-            Circular velocity at each given `R`
-
-        Notes
-        -----
-        This function determines the circular velocity as a function of radius for
-        a massive Gaussian ring component with a total mass, `total_mass`,
-        and a ring peak radius to ring FWHM ratio, `invh`.
-        This uses numerically calculated lookup tables.
-
-        """
-        scale_fac = np.sqrt(total_mass / self.tab_mass) * np.sqrt(self.tab_R_peak / R_peak)
-        vcirc = self.vcirc_interp(R / R_peak * self.tab_R_peak) * scale_fac
-
-        # scale_fac = np.sqrt(total_mass / table_mass) * np.sqrt(table_Rpeak / R_peak)
-        # vcirc_interp = v_interp(Rarr / R_peak * table_Rpeak) * scale_fac
-
-        return vcirc
+    # def vcirc(self, R, R_peak, total_mass):
+    #     """
+    #     Calculate circular velocity for a inf thin massive gaussian Ring
+    #
+    #     Parameters
+    #     ----------
+    #     R : float or array
+    #         Radius or radii at which to calculate the circular velocity in kpc
+    #
+    #     R_peak : float
+    #         Peak of Gaussian ring in kpc
+    #
+    #     total_mass : float
+    #         Total mass of the Gaussian ring component
+    #
+    #     Returns
+    #     -------
+    #     vcirc : float or array
+    #         Circular velocity at each given `R`
+    #
+    #     Notes
+    #     -----
+    #     This function determines the circular velocity as a function of radius for
+    #     a massive Gaussian ring component with a total mass, `total_mass`,
+    #     and a ring peak radius to ring FWHM ratio, `invh`.
+    #     This uses numerically calculated lookup tables.
+    #
+    #     """
+    #     scale_fac = np.sqrt(total_mass / self.tab_mass) * np.sqrt(self.tab_R_peak / R_peak)
+    #     vcirc = self.vcirc_interp(R / R_peak * self.tab_R_peak) * scale_fac
+    #
+    #     # scale_fac = np.sqrt(total_mass / table_mass) * np.sqrt(table_Rpeak / R_peak)
+    #     # vcirc_interp = v_interp(Rarr / R_peak * table_Rpeak) * scale_fac
+    #
+    #     return vcirc
 
 
     def potential_gradient(self, R, R_peak, total_mass):
@@ -2578,11 +2597,10 @@ class GaussianRing(MassModel, _LightMassModel):
     mass_to_light = DysmalParameter(default=1, fixed=True)
 
     _subtype = 'baryonic'
-    _potential_gradient_has_neg = True
 
     def __init__(self, baryon_type='gas+stars', **kwargs):
-
         self.baryon_type = baryon_type
+
         super(GaussianRing, self).__init__(**kwargs)
 
         self._initialize_ring_table()
@@ -2607,9 +2625,6 @@ class GaussianRing(MassModel, _LightMassModel):
     def _update_ring_table(self):
         if self.ring_invh() != self.ring_table._invh:
             self.ring_table.invh = self.ring_invh()
-
-    # def ring_FWHM(self):
-    #     return self.sigma_R.value * (2.*np.sqrt(2.*np.log(2.)))
 
     def sigma_R(self):
         return self.FWHM.value / (2.*np.sqrt(2.*np.log(2.)))
@@ -2666,14 +2681,38 @@ class GaussianRing(MassModel, _LightMassModel):
         vcirc : float or array
             Circular velocity in km/s
         """
-        # Check invh is correct
-        self._update_ring_table()
+        # # Check invh is correct
+        # self._update_ring_table()
 
-        return self.ring_table.vcirc(r, self.R_peak.value, 10**self.total_mass.value)
+        # return self.ring_table.vcirc(r, self.R_peak.value, 10**self.total_mass.value)
+
+        return np.sqrt(self.vcirc_sq(r))
+
+    def vcirc_sq(self, r):
+        """
+        Square of circular velocity as a function of radius
+
+        Parameters
+        ----------
+        r : float or array
+            Radii at which to calculate the enclosed mass
+
+        Returns
+        -------
+        vcirc_sq : float or array
+            Square of circular velocity in km^2/s^2
+
+        Notes
+        -----
+        Calculated as :math:`v_{\mathrm{circ}}^2(R) = R * \partial \Phi / \partial R`
+        from the gradient of the potential, as the potential gradient has negative values.
+        """
+        return r * self.potential_gradient(r)
+
 
     def potential_gradient(self, r):
         r"""
-        Default method to evaluate the gradient of the potential, :math:`\del\Phi(r)/\del r`.
+        Method to evaluate the gradient of the potential, :math:`\del\Phi(r)/\del r`.
 
         Parameters
         ----------
@@ -2685,15 +2724,6 @@ class GaussianRing(MassModel, _LightMassModel):
         dPhidr : float or array
             Gradient of the potential at `r`
 
-        Notes
-        -----
-        Calculates the gradient of the potential from the circular velocity
-        using :math:`\del\Phi(r)/\del r = v_{c}^2(r)/r`.
-        An alternative should be written for components where the
-        potential gradient is ever *negative* (i.e., rings).
-
-        Can be coupled with setting & checking `model._potential_gradient_has_neg` flag
-        for mass models.
         """
         # Check invh is correct
         self._update_ring_table()

dysmalpy/models/model_set.py
@@ -1069,11 +1069,67 @@ class ModelSet:
             return enc_mass
 
 
-
     def circular_velocity(self, r, compute_baryon=False, compute_dm=False,
                             model_key_re=['disk+bulge', 'r_eff_disk'], step1d=0.2):
         """
         Calculate the total circular velocity as a function of radius
+
+        Parameters
+        ----------
+        r : float or array
+            Radius or radii at which to calculate the circular velocity in kpc
+
+        compute_baryon : bool
+            If True, also return the circular velocity due to the baryons
+
+        compute_dm : bool
+            If True, also return the circular velocity due to the halo
+
+        model_key_re : list, optional
+            Two element list which contains the name of the model component
+            and parameter to use for the effective radius.
+            Default is ['disk+bulge', 'r_eff_disk'].
+
+        step1d : float, optional
+            Step size in kpc to use during adiabatic contraction calculation
+
+        Returns
+        -------
+        vel : float or array
+            Total circular velocity in km/s
+
+        vbaryon : float or array, only if `compute_baryon` = True
+            Circular velocity due to the baryons
+
+        vdm : float or array, only if `compute_dm` = True
+            Circular velocity due to the halo
+        """
+
+        vels_sq = self.vcirc_sq(r, compute_baryon=compute_baryon, compute_dm=compute_dm,
+                                model_key_re=model_key_re, step1d=step1d)
+
+        if (compute_baryon and compute_dm):
+            vel = np.sqrt(vels_sq[0])
+            vbaryon = np.sqrt(vels_sq[1])
+            vdm = np.sqrt(vels_sq[2])
+            return vel, vbaryon, vdm
+        elif (compute_dm and (not compute_baryon)):
+            vel = np.sqrt(vels_sq[0])
+            vdm = np.sqrt(vels_sq[1])
+            return vel, vdm
+        elif (compute_baryon and (not compute_dm)):
+            vel = np.sqrt(vels_sq[0])
+            vbaryon = np.sqrt(vels_sq[1])
+            return vel, vbaryon
+        else:
+            vel = np.sqrt(vels_sq)
+            return vel
+
+
+    def vcirc_sq(self, r, compute_baryon=False, compute_dm=False,
+                            model_key_re=['disk+bulge', 'r_eff_disk'], step1d=0.2):
+        """
+        Calculate the square of the total circular velocity as a function of radius
 
         Parameters
         ----------
@@ -1122,10 +1178,7 @@ class ModelSet:
                 if self.mass_components[cmp]:
                     mcomp = self.components[cmp]
 
-                    if mcomp._potential_gradient_has_neg:
-                        cmpnt_v_sq = r * mcomp.potential_gradient(r)
-                    else:
-                        cmpnt_v_sq = mcomp.circular_velocity(r) **2
+                    cmpnt_v_sq = mcomp.vcirc_sq(r)
 
                     if (mcomp._subtype == 'dark_matter') | (mcomp._subtype == 'combined'):
                         #vdm = np.sqrt(vdm ** 2 + cmpnt_v ** 2)
@@ -1148,28 +1201,26 @@ class ModelSet:
             #                                                        model_key_re=model_key_re,
             #                                                        step1d=step1d)
 
-            vels = self.kinematic_options.apply_adiabatic_contract(self, r, vbaryon_sq, vdm_sq,
+            vels_sq = self.kinematic_options.apply_adiabatic_contract(self, r, vbaryon_sq, vdm_sq,
                                                                    compute_dm=compute_dm,
+                                                                   return_vsq=True,
                                                                    model_key_re=model_key_re,
                                                                    step1d=step1d)
 
-            if compute_baryon:
-                vbaryon = np.sqrt(vbaryon_sq)
-
             if compute_dm:
-                vel = vels[0]
-                vdm = vels[1]
+                vel_sq = vels_sq[0]
+                vdm_sq = vels_sq[1]
             else:
-                vel = vels
+                vel_sq = vels_sq
 
             if (compute_baryon and compute_dm):
-                return vel, vbaryon, vdm
+                return vel_sq, vbaryon_sq, vdm_sq
             elif (compute_dm and (not compute_baryon)):
-                return vel, vdm
+                return vel_sq, vdm_sq
             elif (compute_baryon and (not compute_dm)):
-                return vel, vbaryon
+                return vel_sq, vbaryon_sq
             else:
-                return vel
+                return vel_sq
 
     def velocity_profile(self, r, compute_dm=False):
         """

dysmalpy/parameters.py
@@ -11,6 +11,7 @@ from __future__ import (absolute_import, unicode_literals, division,
 import abc
 import operator
 import functools
+import copy
 
 # Third party
 import numpy as np
@@ -21,7 +22,8 @@ import six
 
 __all__ = ['DysmalParameter', 'Prior', 'UniformPrior', 'GaussianPrior',
            'BoundedGaussianPrior', 'BoundedGaussianLinearPrior',
-           'BoundedSineGaussianPrior', 'UniformLinearPrior']
+           'BoundedSineGaussianPrior', 'UniformLinearPrior',
+           'ConditionalUniformPrior', 'ConditionalEmpiricalUniformPrior']
 
 
 def _binary_comparison_operation(op):
@@ -46,6 +48,80 @@ def _binary_comparison_operation(op):
 
     return wrapper
 
+
+
+def _f_cond_boundaries(param, modelset, f_bounds):
+    """
+    Default function f_cond(param, modelset, f_bounds) that takes the parameter and model set as input,
+        as well as the boundary function for the conditions.
+        It must return True/False if the value does / does not satisfy the conditional requirements.
+    """
+
+    # Get boundaries for current param / model values:
+    pmin, pmax = f_bounds(param, modelset)
+
+    if (param.value >= pmin) & (param.value <= pmax):
+        return True
+    else:
+        return False
+
+def _f_boundaries_from_cond(param, modelset, f_cond):
+    """
+    Default function f_bounds(param, modelset, f_cond) that takes the parameter and model set as input,
+        as well as the conditional function.
+        It will return pmin, pmax that are empirically determined based on conditions
+            (up to some rough lower/upper limits).
+    This should only be called to initialize walkers, so hopefully it should be robust against
+        slight limit inaccuracies.
+    """
+    if (param.bounds[0] is None) or (param.bounds[0] == -np.inf):
+        pmin_pb = -1.e2     # Need to default to a finite value
+    else:
+        pmin_pb = param.bounds[0]
+
+    if (param.bounds[1] is None) or (param.bounds[1] == np.inf):
+        pmax_pb = 1.e2     # Need to default to a finite value
+    else:
+        pmax_pb = param.bounds[1]
+
+
+    origstepsize = 0.01
+    Nsteps_max = 1001
+    Nsteps = np.min([np.int(np.round((pmax_pb-pmin_pb)/origstepsize)), Nsteps_max])
+    stepsize = (pmax_pb-pmin_pb)/(1.*Nsteps)
+    parr = np.arange(pmin_pb, pmax_pb+stepsize, stepsize)
+    condarr = np.zeros(len(parr), dtype=bool)
+
+    rarr = np.arange(0., 15.1, 0.1)
+
+    mod = copy.deepcopy(modelset)
+
+    for i, p in enumerate(parr):
+        # Update the param values:
+        mod.set_parameter_value(param._model._name, param._name, p)
+        condarr[i] = np.all(np.isfinite(mod.circular_velocity(rarr)))
+
+    # Defaults:
+    pmin = pmin_pb
+    pmax = pmax_pb
+    if not np.all(condarr):
+        condarr_int = np.array(condarr, dtype=int)
+        condarr_delts = condarr_int[1:]-condarr_int[:-1]
+        wh_pos = np.where(condarr_delts > 0)[0]
+        wh_neg = np.where(condarr_delts < 0)[0]
+        if len(wh_pos) > 0:
+            ind = wh_pos[-1]+1
+            pmin = parr[ind]
+        if len(wh_neg) > 0:
+            ind = wh_neg[-1]-1
+            if ind >= 0:
+                pmax = parr[ind]
+            else:
+                # Probably suspect.... but default to minimum....
+                pmax = parr[0]
+                pmin -= 1.
+
+    return pmin, pmax
 
 # ******* PRIORS ************
 # Base class for priors
@@ -102,7 +178,7 @@ class UniformPrior(Prior):
             return -np.inf
 
     @staticmethod
-    def sample_prior(param, N=1):
+    def sample_prior(param, N=1, **kwargs):
         """
         Returns a random sample of parameter values distributed according to the prior
 
@@ -154,7 +230,7 @@ class UniformLinearPrior(Prior):
             return -np.inf
 
     @staticmethod
-    def sample_prior(param, N=1):
+    def sample_prior(param, N=1, **kwargs):
         if param.bounds[0] is None:
             pmin = -1.e13  # Need to default to a finite value for the rand dist.
         else:
@@ -210,7 +286,7 @@ class GaussianPrior(Prior):
         return np.log(norm.pdf(param.value, loc=self.center,
                         scale=self.stddev))
 
-    def sample_prior(self, param, N=1):
+    def sample_prior(self, param, N=1, **kwargs):
         """
         Returns a random sample of parameter values distributed according to the prior
 
@@ -283,7 +359,7 @@ class BoundedGaussianPrior(Prior):
         else:
             return -np.inf
 
-    def sample_prior(self, param, N=1):
+    def sample_prior(self, param, N=1, **kwargs):
         """
         Returns a random sample of parameter values distributed according to the prior
 
@@ -346,7 +422,7 @@ class BoundedGaussianLinearPrior(Prior):
         else:
             return -np.inf
 
-    def sample_prior(self, param, N=1):
+    def sample_prior(self, param, N=1, **kwargs):
 
         if param.bounds[0] is None:
             pmin = -np.inf
@@ -425,7 +501,7 @@ class BoundedSineGaussianPrior(Prior):
         else:
             return -np.inf
 
-    def sample_prior(self, param, N=1):
+    def sample_prior(self, param, N=1, **kwargs):
         """
         Returns a random sample of parameter values distributed according to the prior
 
@@ -463,6 +539,186 @@ class BoundedSineGaussianPrior(Prior):
                 rvs.append(test_v)
 
         return rvs
+
+
+class ConditionalUniformPrior(Prior):
+    """
+    Object for flat priors, but with boundaries that are conditional on other model parameters.
+    """
+
+    def __init__(self, f_bounds=None, f_cond=_f_cond_boundaries):
+        """
+        Initialize ConditionalUniformPrior instance.
+
+        Parameters
+        ----------
+        f_bounds : function
+            Function f_bounds(param, modelset) that takes the parameter and model set as input.
+            It must return a 2-element array with the lower, upper bounds for the parameter,
+            for the given other model parameter settings.
+            These will then be used to uniformly sample the parameter within these bounds.
+
+            Note this will not be perfect, given the other parameters will be perturbed within their priors
+                (and thus some of the sampled value tuples may end up in the bad region), but hopefully
+                the MCMC walkers will still be able to work with this.
+
+        f_cond : function, optional
+            Function f_cond(param, modelset, self.f_bounds) that takes the parameter and model set as input.
+            It must return True/False if the value does / does not satisfy the conditional requirements.
+            If True, then the log prior will be 0., if False, then it will be -np.inf
+
+            If not set, it will default to a conditional based on the boundary function.
+
+        """
+        self.f_bounds = f_bounds
+        self.f_cond = f_cond
+
+    def log_prior(self, param, modelset=None, **kwargs):
+        """
+        Returns the log value of the prior given the parameter value
+
+        Parameters
+        ----------
+        param : `~dysmalpy.parameters.DysmalParameter`
+            `~dysmalpy.parameters.DysmalParameter` object for which the prior is associated to
+        modelset :
+
+        Returns
+        -------
+        lprior : `0` or `-np.inf`
+            Log prior value. 0 if the parameter value is within the bounds otherwise `-np.inf`
+        """
+        if modelset is None:
+            raise ValueError("Must pass `modelset` when calling log_prior() for ConditionalUniformPrior!")
+
+        if (self.f_cond(param, modelset, self.f_bounds)):
+            return 0.
+        else:
+            return -np.inf
+
+    def sample_prior(self, param, N=1, modelset=None, **kwargs):
+        """
+        Returns a random sample of parameter values distributed according to the prior
+
+        Parameters
+        ----------
+        param : `~dysmalpy.parameters.DysmalParameter`
+            `~dysmalpy.parameters.DysmalParameter` object for which the prior is associated to
+        N : int, optional
+            Size of random sample. Default is 1.
+
+        Returns
+        -------
+        rsamp : float or array
+            Random sample of parameter values
+
+        """
+        if modelset is None:
+            raise ValueError("Must pass `modelset` when calling sample_prior() for ConditionalUniformPrior!")
+
+        pmin, pmax = self.f_bounds(param, modelset)
+
+        # Catch infs -- move to small/large, but finite, values:
+        if (pmin is None) or (pmin == -np.inf):
+            pmin = -1.e20  # Need to default to a finite value for the rand dist.
+
+        if (pmax is None) or (pmax == np.inf):
+            pmax = 1.e20  # Need to default to a finite value for the rand dist.
+
+        return np.random.rand(N)*(pmax-pmin) + pmin
+
+
+
+class ConditionalEmpiricalUniformPrior(Prior):
+    """
+    Object for flat priors, but with boundaries that are conditional on other model parameters.
+    Determined through empirical testing of f_cond, and bounds are then inferred based on
+    iterating f_cond.
+    """
+
+    def __init__(self, f_cond=None, f_bounds=_f_boundaries_from_cond):
+        """
+        Initialize ConditionalEmpiricalUniformPrior instance.
+
+        Parameters
+        ----------
+
+        f_cond : function
+            Function f_cond(param, modelset, self.f_bounds) that takes the parameter and model set as input.
+            It must return True/False if the value does / does not satisfy the conditional requirements.
+            If True, then the log prior will be 0., if False, then it will be -np.inf
+
+        f_bounds : function, optional
+            Function f_bounds(param, modelset, self.f_cond) that takes the parameter and model set as input.
+            It must return a 2-element array with the lower, upper bounds for the parameter,
+            for the given other model parameter settings.
+            These will then be used to uniformly sample the parameter within these bounds.
+
+            Note this will not be perfect, given the other parameters will be perturbed within their priors
+                (and thus some of the sampled value tuples may end up in the bad region), but hopefully
+                the MCMC walkers will still be able to work with this.
+
+            If not set, it will default to a function that iterates
+                to find boundaries based on self.f_cond.
+
+        """
+        self.f_cond = f_cond
+        self.f_bounds = f_bounds
+
+    def log_prior(self, param, modelset=None, **kwargs):
+        """
+        Returns the log value of the prior given the parameter value
+
+        Parameters
+        ----------
+        param : `~dysmalpy.parameters.DysmalParameter`
+            `~dysmalpy.parameters.DysmalParameter` object for which the prior is associated to
+        modelset :
+
+        Returns
+        -------
+        lprior : `0` or `-np.inf`
+            Log prior value. 0 if the parameter value is within the bounds otherwise `-np.inf`
+        """
+        if modelset is None:
+            raise ValueError("Must pass `modelset` when calling log_prior() for ConditionalUniformPrior!")
+
+        if (self.f_cond(param, modelset)):
+            return 0.
+        else:
+            return -np.inf
+
+    def sample_prior(self, param, N=1, modelset=None, **kwargs):
+        """
+        Returns a random sample of parameter values distributed according to the prior
+
+        Parameters
+        ----------
+        param : `~dysmalpy.parameters.DysmalParameter`
+            `~dysmalpy.parameters.DysmalParameter` object for which the prior is associated to
+        N : int, optional
+            Size of random sample. Default is 1.
+
+        Returns
+        -------
+        rsamp : float or array
+            Random sample of parameter values
+
+        """
+        if modelset is None:
+            raise ValueError("Must pass `modelset` when calling sample_prior() for ConditionalUniformPrior!")
+
+        pmin, pmax = self.f_bounds(param, modelset, self.f_cond)
+
+        # Catch infs -- move to small/large, but finite, values:
+        if (pmin is None) or (pmin == -np.inf):
+            pmin = -1.e20  # Need to default to a finite value for the rand dist.
+
+        if (pmax is None) or (pmax == np.inf):
+            pmax = 1.e20  # Need to default to a finite value for the rand dist.
+
+        return np.random.rand(N)*(pmax-pmin) + pmin
+
 
 
 class DysmalParameter(Parameter):