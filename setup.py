1645a2b93560019ad285d907e9f011c5ff339403
Sedona Price <sedona.price@gmail.com>
2022-02-03 14:02:48 +0100

    Win syntax setup fixes

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@
 import os
 import re
 
+import logging
+from setuptools.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+
 try:
     from setuptools import setup, Extension
 except:
@@ -19,6 +23,10 @@ mo = re.search(VERS, init_string, re.M)
 __version__ = mo.group(1)
 
 
+logging.basicConfig()
+log = logging.getLogger(__file__)
+
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
@@ -28,6 +36,9 @@ requirements = ['numpy', 'scipy', 'matplotlib', 'astropy',
                 'h5py', 'pandas', 'six']
 
 setup_requirements = ['Cython', 'numpy']
+
+try:
+    # try building with c code :
 
 setup(
     author="Taro Shimizu & Sedona Price",
@@ -54,8 +65,8 @@ setup(
     package_data={'dysmalpy': ['data/noordermeer/*.save']},
     version=__version__,
     ext_modules=cythonize([
-                    "dysmalpy/models/cutils.pyx", 
-                    Extension("dysmalpy.lensingTransformer", 
+                    "dysmalpy/models/cutils.pyx",
+                    Extension("dysmalpy.lensingTransformer",
                         sources=["dysmalpy/lensing_transformer/lensingTransformer.cpp"],
                         language="c++",
                         include_dirs=["lensing_transformer", "/usr/include", "/usr/local/include"],
@@ -63,8 +74,8 @@ setup(
                         lib_dirs=["/usr/lib", "/usr/lib/x86_64-linux-gnu", "/usr/local/lib"],
                         depends=["dysmalpy/lensing_transformer/lensingTransformer.hpp"],
                         extra_compile_args=['-std=c++11']
-                    ), 
-                    Extension("dysmalpy.leastChiSquares1D", 
+                    ),
+                    Extension("dysmalpy.leastChiSquares1D",
                         sources=["dysmalpy/utils_least_chi_squares_1d_fitter/leastChiSquares1D.cpp"],
                         language="c++",
                         include_dirs=["utils_least_chi_squares_1d_fitter", "/usr/include", "/usr/local/include"],
@@ -76,3 +87,42 @@ setup(
                     )
                  ])
 )
+except ext_errors as ex:
+    log.warn(ex)
+    log.warn("The C extension could not be compiled")
+
+    ## Retry to install the module without C extensions :
+    # Remove any previously defined build_ext command class.
+    if 'build_ext' in setup_args['cmdclass']:
+        del setup_args['cmdclass']['build_ext']
+
+    # If this new 'setup' call don't fail, the module
+    # will be successfully installed, without the C extension :
+
+    setup(
+        author="Taro Shimizu & Sedona Price",
+        author_email='shimizu@mpe.mpg.de',
+        python_requires='>=3.5',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: 3-clause BSD',
+            'Natural Language :: English',
+            "Topic :: Scientific/Engineering",
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ],
+        description="A modelling and fitting package for galaxy kinematics.",
+        install_requires=requirements,
+        setup_requires=setup_requirements,
+        license="3-clause BSD",
+        long_description=readme,
+        include_package_data=True,
+        name='dysmalpy',
+        packages=['dysmalpy', 'dysmalpy.extern', 'dysmalpy.models', 'dysmalpy.fitting', 'dysmalpy.fitting_wrappers'],
+        package_data={'dysmalpy': ['data/noordermeer/*.save']},
+        version=__version__,
+        ext_modules=cythonize(["dysmalpy/models/cutils.pyx"])
+    )
+    log.info("Plain installation succeeded.")
