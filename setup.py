2dfe568c2ecf0f581b191e90793dd724a0b12d6f
Sedona Price <sedona.price@gmail.com>
2023-11-30 14:10:00 -0500

    Remove sq aperture; setup.cfg
    Working on setup.py + setup.cfg changes -- IN PROGRESS.

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Imports #
 import os
 import re
 
-import logging
 
-try:
-    from setuptools import setup, Extension
-except:
-    from distutils.core import setup, Extension
-
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+import logging
 
+# Find version #
+dir_path = os.path.dirname(os.path.realpath(__file__))
 init_string = open(os.path.join(dir_path, 'dysmalpy', '__init__.py')).read()
 VERS = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VERS, init_string, re.M)
@@ -28,39 +26,32 @@ log = logging.getLogger(__file__)
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = ['numpy', 'scipy', 'matplotlib', 'astropy',
-                'emcee', 'corner', 'cython', 'dill',
-                'shapely', 'photutils', 'spectral-cube', 'radio-beam',
-                'h5py', 'pandas', 'six']
+
 
 setup_requirements = ['Cython', 'numpy']
 
-
-setup_args = {'name': 'dysmalpy',
+# Setup #
+setup_args = {
+        'name': 'dysmalpy',
         'author': "MPE IR/Sub-mm Group",
         'author_email': 'shimizu@mpe.mpg.de',
-        'python_requires': '>=3.10',
-        'classifiers': [
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: 3-clause BSD',
-            'Natural Language :: English',
-            "Topic :: Scientific/Engineering",
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-        ],
         'description': "A modelling and fitting package for galaxy kinematics.",
-        'install_requires': requirements,
-        'setup_requires': setup_requirements,
-        'license': "3-clause BSD",
         'long_description': readme,
+        'url': "https://github.com/ttshimiz/dysmalpy",
         'include_package_data': True,
-        'packages': ['dysmalpy', 'dysmalpy.extern', 'dysmalpy.models',
-                     'dysmalpy.fitting', 'dysmalpy.fitting_wrappers',
-                     'tests'],
-        'package_data': {'dysmalpy': ['data/noordermeer/*.save', 'data/deprojected_sersic_models_tables/*.fits'],
-                         'tests': ['test_data/*', 'test_data_lensing/*', 'test_data_masking/*', 'test_data_masking/*']},
-        'version': __version__ }
+        'packages': find_packages(), 
+        'setup_requires': setup_requirements,
+        'version': __version__, 
+        'package_data': {
+            'dysmalpy': [
+                'data/deprojected_sersic_models_tables/*.fits'
+            ],
+            'tests': [
+                'test_data/*', 
+                'test_data_lensing/*', 
+                'test_data_masking/*'
+            ]},
+        }
 
 
 # Add CONDA include and lib paths if necessary
@@ -73,25 +64,21 @@ if 'CONDA_PREFIX' in os.environ:
     log.debug('conda_lib_path: {!r}'.format(conda_lib_path))
 
 
-# Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
-import distutils.sysconfig
-cfg_vars = distutils.sysconfig.get_config_vars()
-for key, value in cfg_vars.items():
-    if type(value) == str:
-        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
 
 
-ext_modules = cythonize([Extension("dysmalpy.models.cutils",
-                                   sources=["dysmalpy/models/cutils.pyx"],
-                                   include_dirs=[conda_include_path, "/usr/include", "/usr/local/include", "/opt/local/include"],
-                                   library_dirs=[conda_lib_path, "/usr/lib", "/usr/lib/x86_64-linux-gnu", "/usr/local/lib", "/opt/local/lib"],
-                        )])
+ext_modules = cythonize([
+    Extension("dysmalpy.models.cutils",
+    sources=["dysmalpy/models/cutils.pyx"],
+    include_dirs=[conda_include_path, "/usr/include", "/usr/local/include", "/opt/local/include"],
+    library_dirs=[conda_lib_path, "/usr/lib", "/usr/lib/x86_64-linux-gnu", "/usr/local/lib", "/opt/local/lib"],
+)])
 ext_modules_optional = cythonize([
-                Extension("dysmalpy.models.cutils",
-                          sources=["dysmalpy/models/cutils.pyx"],
-                          include_dirs=[conda_include_path, "/usr/include", "/usr/local/include", "/opt/local/include"],
-                           library_dirs=[conda_lib_path, "/usr/lib", "/usr/lib/x86_64-linux-gnu", "/usr/local/lib", "/opt/local/lib"],
-                          ),
+    Extension("dysmalpy.models.cutils",
+    sources=["dysmalpy/models/cutils.pyx"],
+    include_dirs=[conda_include_path, "/usr/include", "/usr/local/include", "/opt/local/include"],
+    library_dirs=[conda_lib_path, "/usr/lib", "/usr/lib/x86_64-linux-gnu", "/usr/local/lib", "/opt/local/lib"],
+)])
+ext_modules_optional.append([
                 Extension("dysmalpy.lensingTransformer",
                     sources=["dysmalpy/lensing_transformer/lensingTransformer.cpp"],
                     language="c++",
@@ -100,7 +87,9 @@ ext_modules_optional = cythonize([
                     library_dirs=[conda_lib_path, "/usr/lib", "/usr/lib/x86_64-linux-gnu", "/usr/local/lib", "/opt/local/lib"],
                     depends=["dysmalpy/lensing_transformer/lensingTransformer.hpp"],
                     extra_compile_args=['-std=c++11'], optional=True
-                ),
+                )
+            ])
+ext_modules_optional.append([
                 Extension("dysmalpy.leastChiSquares1D",
                     sources=["dysmalpy/utils_least_chi_squares_1d_fitter/leastChiSquares1D.cpp"],
                     language="c++",
@@ -111,7 +100,7 @@ ext_modules_optional = cythonize([
                             "dysmalpy/utils_least_chi_squares_1d_fitter/leastChiSquaresFunctions1D.hpp"],
                     extra_compile_args=['-std=c++11'], optional=True
                 )
-             ])
+            ])
 
 
 try:
